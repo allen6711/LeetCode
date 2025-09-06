55d73a61797129788def528fe50b1af65f5dcdce
allen6711 <allen2990167@gmail.com>
2025-09-05 19:24:57 -0700

    Time: 0 ms (100%), Space: 17.8 MB (84.26%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -0,0 +1,68 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        
+        queue = deque([(root.left, root.right)])
+        
+        while queue:
+            root_l, root_r = queue.popleft()
+
+            if not root_l and not root_r:
+                continue
+            
+            if not root_l or not root_r:
+                return False
+            
+            if root_l.val != root_r.val:
+                return False
+            
+            queue.append((root_l.left, root_r.right))
+            queue.append((root_l.right, root_r.left))      
+
+        return True
+
+
+
+        # DFS
+        # def isMirror(a: Optional[TreeNode], b: Optional[TreeNode]) -> bool:
+        #     if not a and not b:
+        #         return True
+        #     if not a or not b:
+        #         return False
+        #     if a.val != b.val:
+        #         return False
+            
+        #     return isMirror(a.left, b.right) and isMirror(a.right, b.left)
+        
+        # if not root:
+        #     return True
+        
+        # return isMirror(root.left, root.right)
+
+
+        # BFS
+        # if not root:
+        #      return True
+
+        # queue = deque([(root.left, root.right)])
+
+        # while queue:
+        #     root_l, root_r = queue.popleft()
+        #     if not root_l and not root_r:
+        #         continue
+        #     if not root_l or not root_r:
+        #         return False
+        #     if root_l.val != root_r.val:
+        #         return False
+
+        #     queue.append((root_l.left, root_r.right))
+        #     queue.append((root_l.right, root_r.left))
+        
+        # return True
