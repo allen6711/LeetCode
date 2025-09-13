2431cc31e83ccb2d275cd1919f513346d4822f23
allen6711 <allen2990167@gmail.com>
2025-09-13 00:19:32 -0700

    Time: 0 ms (100%), Space: 18.1 MB (78.37%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.py
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.py
@@ -8,17 +8,64 @@ class Solution:
         while start + 1 < end:
             mid = (start + end) // 2
 
-            # since the nums[-1] might be same as other element, we should dynamic update
+            # [2, 2, 2, 0, 1, 2]
             if nums[mid] < nums[end]:
                 end = mid
             
             elif nums[mid] > nums[end]:
                 start = mid
             
-            else:    # nums[mid] == nums[-1], we are not sure the position
+            else:
                 end -= 1
             
         return min(nums[start], nums[end])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if not nums:
+        #     return -1
+        
+        # start, end = 0, len(nums) - 1
+
+        # while start + 1 < end:
+        #     mid = (start + end) // 2
+
+        #     # since the nums[-1] might be same as other element, we should dynamic update
+        #     if nums[mid] < nums[end]:
+        #         end = mid
+            
+        #     elif nums[mid] > nums[end]:
+        #         start = mid
+            
+        #     else:    # nums[mid] == nums[-1], we are not sure the position
+        #         end -= 1
+            
+        # return min(nums[start], nums[end])
                 
         # brute-force
         # min = float('inf')
