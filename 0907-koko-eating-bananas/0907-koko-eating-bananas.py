9a11ab310fbb773d915b1952ba932bba3a748634
allen6711 <allen2990167@gmail.com>
2025-09-14 16:14:40 -0700

    Time: 144 ms (88.01%), Space: 19.3 MB (6.66%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -0,0 +1,80 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # piles: [5, 6, 8]
+        # h hours
+        # eating speed: bananas/hr
+        # hours in pile: ceil(pile/h)
+        # as slow as she can: minimum k in h
+        # k: 1,         2, 3, ..., 8
+        # h: 19=sum(piles),        3
+        # condition: smaller than h
+
+        start, end = 1, max(piles)
+
+        while start + 1 < end:
+            mid = (start + end) // 2
+
+            if self.countHours(piles, mid) <= h:
+                end = mid
+            
+            else:
+                start = mid
+        
+        if self.countHours(piles, start) <= h:
+            return start
+
+        if self.countHours(piles, end) <= h:
+            return end
+
+
+    def countHours(self, piles, speed):
+        hours = 0
+
+        for pile in piles:
+            hours += ceil(pile / speed)
+        
+        return hours
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # left, right = 1, int(1e9)
+        # while left < right:
+        #     mid = (left + right) // 2
+        #     total_hours = 0
+            
+        #     for pile in piles:
+        #         total_hours += (pile + mid - 1) // mid
+            
+        #     if total_hours <= h:
+        #         right = mid
+        #     else:
+        #         left = mid + 1
+        
+        # return left
