ce122ee3dee9d1e3a5b68e31c376847cb8fc429d
allen6711 <allen2990167@gmail.com>
2025-09-14 15:31:44 -0700

    Time: 0 ms (100%), Space: 18 MB (38.29%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -8,12 +8,15 @@ class Solution:
         while start + 1 < end:
             mid = (start + end) // 2
 
+            # left side of peak
             if nums[mid - 1] < nums[mid]:
                 start = mid
             
-            elif nums[mid - 1] > nums[mid]:
+            elif nums[mid] < nums[mid - 1]:
                 end = mid
             
+            # elif nums[mid - 1] < nums[mid] < nums[mid + 1]:
+            #     return mid
             else:
                 return mid
         
@@ -22,21 +25,37 @@ class Solution:
         
         else:
             return end
-        
+
         return -1
 
 
 
 
 
+        # if not nums:
+        #     return -1
+        
+        # start, end = 0, len(nums) - 1
 
+        # while start + 1 < end:
+        #     mid = (start + end) // 2
 
-
-
-
-
-
-
+        #     if nums[mid - 1] < nums[mid]:
+        #         start = mid
+            
+        #     elif nums[mid - 1] > nums[mid]:
+        #         end = mid
+            
+        #     else:
+        #         return mid
+        
+        # if nums[start] > nums[end]:
+        #     return start
+        
+        # else:
+        #     return end
+        
+        # return -1
 
 
 
