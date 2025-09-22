cd92f379bc895fbc4d5cd4ce539c8f518de6245c
allen6711 <allen2990167@gmail.com>
2025-09-21 21:40:06 -0700

    Time: 0 ms (100%), Space: 18 MB (24.84%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -6,28 +6,45 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        # Traverse
+        # Divide and Conquer
         if root is None:
             return []
 
+        if root.left is None and root.right is None:
+            return [str(root.val)]
+        
+        leftPaths = self.binaryTreePaths(root.left)
+        rightPaths = self.binaryTreePaths(root.right)
+
         paths = []
-        results = []
-        self.dfs(root, [str(root.val)], results)
 
-        return results
+        for path in leftPaths + rightPaths:
+            paths.append(str(root.val) + '->' + path)
+        
+        return paths
 
-    def dfs(self, node, paths, results):
-        if node.left is None and node.right is None:
-            results.append('->'.join(paths))
-            return
+        # # Traverse
+        # if root is None:
+    #         return []
 
-        if node.left:
-            paths.append(str(node.left.val))
-            self.dfs(node.left, paths, results)
-            paths.pop()
+    #     paths = []
+    #     results = []
+    #     self.dfs(root, [str(root.val)], results)
 
-        if node.right:
-            paths.append(str(node.right.val))
-            self.dfs(node.right, paths, results)
-            paths.pop()
+    #     return results
+
+    # def dfs(self, node, paths, results):
+    #     if node.left is None and node.right is None:
+    #         results.append('->'.join(paths))
+    #         return
+
+    #     if node.left:
+    #         paths.append(str(node.left.val))
+    #         self.dfs(node.left, paths, results)
+    #         paths.pop()
+
+    #     if node.right:
+    #         paths.append(str(node.right.val))
+    #         self.dfs(node.right, paths, results)
+    #         paths.pop()
                 
