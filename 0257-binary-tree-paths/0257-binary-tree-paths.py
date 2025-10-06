c6b8753e42c00eb42957107c51d1a8efe424ed30
allen6711 <allen2990167@gmail.com>
2025-10-05 19:30:04 -0700

    Time: 0 ms (100%), Space: 17.7 MB (92.62%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -6,40 +6,50 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        # Divide and Conquer
+        # Traversal
         if root is None:
             return []
 
-        if root.left is None and root.right is None:
-            return [str(root.val)]
-
-        leftPaths = self.binaryTreePaths(root.left)
-        rightPaths = self.binaryTreePaths(root.right)
-
+        results = []
         paths = []
+        self.dfs(root, [str(root.val)], results)
 
-        for path in leftPaths + rightPaths:
-            paths.append(str(root.val) + "->" + path)
+        return results
+        
+    
+    def dfs(self, node, paths, results):
+        if node.left is None and node.right is None:
+            results.append('->'.join(paths))
+            return
 
-        return paths
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if node.left:
+            paths.append(str(node.left.val))
+            self.dfs(node.left, paths, results)
+            paths.pop()
+        
+        if node.right:
+            paths.append(str(node.right.val))
+            self.dfs(node.right, paths, results)
+            paths.pop()
 
 
+
+        # Divide and Conquer
+        # if root is None:
+        #     return []
+
+        # if root.left is None and root.right is None:
+        #     return [str(root.val)]
+
+        # leftPaths = self.binaryTreePaths(root.left)
+        # rightPaths = self.binaryTreePaths(root.right)
+
+        # paths = []
+
+        # for path in leftPaths + rightPaths:
+        #     paths.append(str(root.val) + "->" + path)
+
+        # return paths
 
 
 
