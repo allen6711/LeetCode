c8a5b005bc89f80c98adb6081ad1e64915ce65b8
allen6711 <allen2990167@gmail.com>
2025-10-06 00:53:10 -0700

    Time: 153 ms (46.67%), Space: 22.7 MB (6.67%) - LeetHub

---
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        p_bool, q_bool, lca = self.helper(root, p, q)
+
+        if p_bool and q_bool:
+            return lca
+        
+        else:
+            return None
+
+    def helper(self, root, p, q):
+        if root is None:
+            return False, False, None
+        
+        left_p, left_q, left_node = self.helper(root.left, p, q)
+        right_p, right_q, right_node = self.helper(root.right, p, q)
+
+        # p exists
+        p_bool = left_p or right_p or root == p
+
+        # q exists
+        q_bool = left_q or right_q or root == q
+
+        if root == p or root == q:
+            return p_bool, q_bool, root
+
+        if left_node is not None and right_node is not None:
+            return p_bool, q_bool, root
+
+        if left_node is not None:
+            return p_bool, q_bool, left_node
+
+        if right_node is not None:
+            return p_bool, q_bool, right_node
+
+        return p_bool, q_bool, None
+
