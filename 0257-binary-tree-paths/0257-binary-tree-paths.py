39a45310bf99f82d5e116e6bea629bec363f1372
allen6711 <allen2990167@gmail.com>
2025-10-22 15:06:50 -0700

    Time: 0 ms (100%), Space: 17.8 MB (72.26%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -8,16 +8,15 @@ class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         if root is None:
             return []
-
+        
         if root.left is None and root.right is None:
             return [str(root.val)]
         
-        left_path = self.binaryTreePaths(root.left)
-        right_path = self.binaryTreePaths(root.right)
-
+        left_paths = self.binaryTreePaths(root.left)
+        right_paths = self.binaryTreePaths(root.right)
         paths = []
 
-        for path in left_path + right_path:
+        for path in left_paths + right_paths:
             paths.append(str(root.val) + "->" + path)
         
         return paths
