0450d0dbff1c8c5dc844c7eb06522945fbcb37b6
allen6711 <allen2990167@gmail.com>
2025-10-22 17:22:15 -0700

    Time: 163 ms (16.24%), Space: 22.6 MB (11.11%) - LeetHub

--- 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.py
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -7,30 +7,30 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        p_bool, q_bool, LCA_node = self.helper(root, p, q)
+        p_bool, q_bool, lca_node = self.helper(root, p, q)
 
-        return LCA_node if p_bool and q_bool else None
+        return lca_node if p_bool and q_bool else None
     
     def helper(self, root, p, q):
         if root is None:
             return False, False, None
         
-        p_left, q_left, left_node = self.helper(root.left, p, q)
-        p_right, q_right, right_node = self.helper(root.right, p, q)
-        
-        p_bool = p_left or p_right or root is p
-        q_bool = q_left or q_right or root is q
-        
+        left_p, left_q, left_node = self.helper(root.left, p, q)
+        right_p, right_q, right_node = self.helper(root.right, p, q)
+
+        p_bool = left_p or right_p or root is p
+        q_bool = left_q or right_q or root is q
+
         if root is p or root is q:
             return p_bool, q_bool, root
         
         if left_node is not None and right_node is not None:
             return p_bool, q_bool, root
         
-        if left_node is not None:
+        if left_node:
             return p_bool, q_bool, left_node
-
-        if right_node is not None:
+        
+        if right_node:
             return p_bool, q_bool, right_node
         
         return p_bool, q_bool, None
@@ -43,8 +43,62 @@ class Solution:
 
 
 
+        
+    #     p_bool, q_bool, lca_node = self.helper(root, p, q)
 
+    #     return lca_node if p_bool and q_bool else None
+    
+    # def helper(self, root, p, q):
+    #     if root is None:
+    #         return False, False, None
+        
+    #     p_left, q_left, left_node = self.helper(root.left, p, q)
+    #     p_right, q_right, right_node = self.helper(root.right, p, q)
 
+    #     p_bool = p_left or p_right or root is p
+    #     q_bool = q_left or q_right or root is q
+
+    #     if root is p or root is q:
+    #         return p_bool, q_bool, root
+        
+    #     if left_node is not None and right_node is not None:
+    #         return p_bool, q_bool, root
+
+    #     if left_node:
+    #         return p_bool, q_bool, left_node
+        
+    #     if right_node:
+    #         return p_bool, q_bool, right_node
+        
+    #     return p_bool, q_bool, None
+
+    #     p_bool, q_bool, LCA_node = self.helper(root, p, q)
+
+    #     return LCA_node if p_bool and q_bool else None
+    
+    # def helper(self, root, p, q):
+    #     if root is None:
+    #         return False, False, None
+        
+    #     p_left, q_left, left_node = self.helper(root.left, p, q)
+    #     p_right, q_right, right_node = self.helper(root.right, p, q)
+        
+    #     p_bool = p_left or p_right or root is p
+    #     q_bool = q_left or q_right or root is q
+        
+    #     if root is p or root is q:
+    #         return p_bool, q_bool, root
+        
+    #     if left_node is not None and right_node is not None:
+    #         return p_bool, q_bool, root
+        
+    #     if left_node is not None:
+    #         return p_bool, q_bool, left_node
+
+    #     if right_node is not None:
+    #         return p_bool, q_bool, right_node
+        
+    #     return p_bool, q_bool, None
 
 
     #     p_bool, q_bool, lca = self.helper(root, p, q)
