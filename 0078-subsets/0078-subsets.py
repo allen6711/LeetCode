25175615732d6e5182f6a2ec44be40d5f49cffb0
allen6711 <allen2990167@gmail.com>
2025-10-22 22:13:49 -0700

    Time: 0 ms (100%), Space: 17.7 MB (90.61%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,28 +2,42 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         combinations = []
         self.dfs(nums, 0, [], combinations)
-        return combinations
 
+        return combinations
+    
     def dfs(self, nums, index, subset, combinations):
         combinations.append(list(subset))
-        
+
         for i in range(index, len(nums)):
             subset.append(nums[i])
             self.dfs(nums, i + 1, subset, combinations)
             subset.pop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #     combinations = []
+    #     self.dfs(nums, 0, [], combinations)
+    #     return combinations
+
+    # def dfs(self, nums, index, subset, combinations):
+    #     combinations.append(list(subset))
+        
+    #     for i in range(index, len(nums)):
+    #         subset.append(nums[i])
+    #         self.dfs(nums, i + 1, subset, combinations)
+    #         subset.pop()
     
-
-
-
-
-
-
-
-
-
-
-
-
 
     #     combinations = []
     #     self.dfs(nums, 0, [], combinations)
