40ea29d8377854d69c37fe26bfa0502f4a176c0a
allen6711 <allen2990167@gmail.com>
2025-10-22 20:21:13 -0700

    Time: 156 ms (36.54%), Space: 22.7 MB (7.26%) - LeetHub

--- 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.py
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -15,16 +15,16 @@ class Solution:
         if root is None:
             return False, False, None
         
-        left_p, left_q, left_node = self.helper(root.left, p, q)
-        right_p, right_q, right_node = self.helper(root.right, p, q)
+        p_left, q_left, left_node = self.helper(root.left, p, q)
+        p_right, q_right, right_node = self.helper(root.right, p, q)
 
-        p_bool = left_p or right_p or root is p
-        q_bool = left_q or right_q or root is q
+        p_bool = p_left or p_right or root is p
+        q_bool = q_left or q_right or root is q
 
         if root is p or root is q:
             return p_bool, q_bool, root
-        
-        if left_node is not None and right_node is not None:
+
+        if left_node and right_node:
             return p_bool, q_bool, root
         
         if left_node:
@@ -41,6 +41,38 @@ class Solution:
 
 
 
+
+
+
+
+
+    #     p_bool, q_bool, lca_node = self.helper(root, p, q)
+
+    #     return lca_node if p_bool and q_bool else None
+    
+    # def helper(self, root, p, q):
+    #     if root is None:
+    #         return False, False, None
+        
+    #     left_p, left_q, left_node = self.helper(root.left, p, q)
+    #     right_p, right_q, right_node = self.helper(root.right, p, q)
+
+    #     p_bool = left_p or right_p or root is p
+    #     q_bool = left_q or right_q or root is q
+
+    #     if root is p or root is q:
+    #         return p_bool, q_bool, root
+        
+    #     if left_node is not None and right_node is not None:
+    #         return p_bool, q_bool, root
+        
+    #     if left_node:
+    #         return p_bool, q_bool, left_node
+        
+    #     if right_node:
+    #         return p_bool, q_bool, right_node
+        
+    #     return p_bool, q_bool, None
 
 
         
