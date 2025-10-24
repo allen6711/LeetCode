cadb929baada49d275df457112fc161a7193d04e
allen6711 <allen2990167@gmail.com>
2025-10-23 17:56:25 -0700

    Time: 88 ms (67.06%), Space: 22 MB (98.47%) - LeetHub

--- 1816-lowest-common-ancestor-of-a-binary-tree-iv/1816-lowest-common-ancestor-of-a-binary-tree-iv.py
+++ 1816-lowest-common-ancestor-of-a-binary-tree-iv/1816-lowest-common-ancestor-of-a-binary-tree-iv.py
@@ -9,7 +9,7 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
         if root is None:
             return None
-        
+
         if root in nodes:
             return root
         
@@ -21,11 +21,31 @@ class Solution:
         
         if left_node:
             return left_node
-        
+
         if right_node:
             return right_node
         
         return None
+
+        # if root is None:
+        #     return None
+        
+        # if root in nodes:
+        #     return root
+        
+        # left_node = self.lowestCommonAncestor(root.left, nodes)
+        # right_node = self.lowestCommonAncestor(root.right, nodes)
+
+        # if left_node and right_node:
+        #     return root
+        
+        # if left_node:
+        #     return left_node
+        
+        # if right_node:
+        #     return right_node
+        
+        # return None
 
 
         # if root is None:
