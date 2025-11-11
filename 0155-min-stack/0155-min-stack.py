c11789ee0a67cdd036df19e0bdcefcf67c825985
allen6711 <allen2990167@gmail.com>
2025-11-10 20:46:54 -0800

    Time: 17 ms (10.65%), Space: 21.3 MB (59.53%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,24 +1,74 @@
 class MinStack:
-
     def __init__(self):
         self.stack = []
         self.minimum = []
-
-    def push(self, val: int) -> None:
+    
+    def push(self, val):
         self.stack.append(val)
         if not self.minimum or val <= self.minimum[-1]:
             self.minimum.append(val)
 
-    def pop(self) -> None:
+    def pop(self):
         main = self.stack.pop()
         if main == self.minimum[-1]:
             self.minimum.pop()
-
-    def top(self) -> int:
+    
+    def top(self):
         return self.stack[-1]
-
-    def getMin(self) -> int:
+    
+    def getMin(self):
         return self.minimum[-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def __init__(self):
+    #     self.stack = []
+    #     self.minimum = []
+
+    # def push(self, val: int) -> None:
+    #     self.stack.append(val)
+    #     if not self.minimum or val <= self.minimum[-1]:
+    #         self.minimum.append(val)
+
+    # def pop(self) -> None:
+    #     main = self.stack.pop()
+    #     if main == self.minimum[-1]:
+    #         self.minimum.pop()
+
+    # def top(self) -> int:
+    #     return self.stack[-1]
+
+    # def getMin(self) -> int:
+    #     return self.minimum[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
