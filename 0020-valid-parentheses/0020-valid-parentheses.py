ebe87e4dd2d2ce86880cf36158ecc4c9ca5fa860
allen6711 <allen2990167@gmail.com>
2025-11-12 12:42:12 -0800

    Time: 3 ms (35.64%), Space: 17.7 MB (86.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,16 +3,30 @@ class Solution:
         expected_dict = {")": "(", "]": "[", "}": "{"}
         stack = []
 
-        for ch in s:
-            if ch in "([{":
-                stack.append(ch)
+        for char in s:
+            if char in "([{":
+                stack.append(char)
             
             else:
-                if not stack or stack[-1] != expected_dict.get(ch, None):
+                if not stack or stack[-1] != expected_dict[char]:
                     return False
                 stack.pop()
         
         return not stack
+
+        # expected_dict = {")": "(", "]": "[", "}": "{"}
+        # stack = []
+
+        # for ch in s:
+        #     if ch in "([{":
+        #         stack.append(ch)
+            
+        #     else:
+        #         if not stack or stack[-1] != expected_dict.get(ch, None):
+        #             return False
+        #         stack.pop()
+        
+        # return not stack
 
 
         # expected_dict = {")": "(", "]": "[", "}": "{"}
