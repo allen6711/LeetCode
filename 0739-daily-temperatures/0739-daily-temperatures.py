4cd728c1ef60818e316689e6d1a0f9e6ca8f3300
allen6711 <allen2990167@gmail.com>
2025-11-11 16:13:14 -0800

    Time: 87 ms (80.59%), Space: 27.4 MB (67.36%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,8 +1,8 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         n = len(temperatures)
-        answer = [0] * n
         index_stack = []
+        answer = [0] * n
 
         for i, temp in enumerate(temperatures):
             while index_stack and temperatures[index_stack[-1]] < temp:
@@ -23,6 +23,20 @@ class Solution:
 
 
 
+
+
+        # n = len(temperatures)
+        # answer = [0] * n
+        # index_stack = []
+
+        # for i, temp in enumerate(temperatures):
+        #     while index_stack and temperatures[index_stack[-1]] < temp:
+        #         j = index_stack.pop()
+        #         answer[j] = i - j
+
+        #     index_stack.append(i)
+        
+        # return answer
 
         # answer = [0] * len(temperatures)
         # stack = []
