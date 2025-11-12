44afd75f08899c9e29df458ec6708893d9abb809
allen6711 <allen2990167@gmail.com>
2025-11-12 14:11:39 -0800

    Time: 182 ms (58.74%), Space: 41.4 MB (24.8%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,15 +1,28 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        sort_car = sorted(zip(position, speed), key = lambda x: -x[0])
         stack = []
+        car_sort = sorted(zip(position, speed), key = lambda x: -x[0])
         fleets = 0
-        max_time = 0
 
-        for pos, spd in sort_car:
+        for pos, spd in car_sort:
             time = (target - pos) / spd
-            if not stack or time > max_time:
+
+            if not stack or time > stack[-1]:
                 stack.append(time)
-                max_time = time
                 fleets += 1
         
         return fleets
+
+        # sort_car = sorted(zip(position, speed), key = lambda x: -x[0])
+        # stack = []
+        # fleets = 0
+        # max_time = 0
+
+        # for pos, spd in sort_car:
+        #     time = (target - pos) / spd
+        #     if not stack or time > max_time:
+        #         stack.append(time)
+        #         max_time = time
+        #         fleets += 1
+        
+        # return fleets
