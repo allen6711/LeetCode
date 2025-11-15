e3fee2ee1847d3467387ca72f7b46f71bb910e56
allen6711 <allen2990167@gmail.com>
2025-11-15 11:00:07 -0800

    Time: 7 ms (89.59%), Space: 17.7 MB (97.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -11,35 +11,19 @@ class Solution:
         
         return all(x == 0 for x in cnt)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        if len(s) != len(t):
-            return False
+        # if len(s) != len(t):
+        #     return False
             
-        cnt = [0] * 26
-        base = ord('a')
+        # cnt = [0] * 26
+        # base = ord('a')
 
-        for char in s:
-            cnt[ord(char) - base] += 1
+        # for char in s:
+        #     cnt[ord(char) - base] += 1
         
-        for char in t:
-            cnt[ord(char) - base] -= 1
+        # for char in t:
+        #     cnt[ord(char) - base] -= 1
         
-        return all(x == 0 for x in cnt)
+        # return all(x == 0 for x in cnt)
 
 
 
