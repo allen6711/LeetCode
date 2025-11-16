a5749fbfb6f0d1595fb4d09b8a1f8c3ec72b35d7
allen6711 <allen2990167@gmail.com>
2025-11-16 11:28:32 -0800

    Time: 0 ms (100%), Space: 17.8 MB (68.84%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -6,28 +6,54 @@ class Solution:
 
         for r in range(9):
             for c in range(9):
-                
                 val = board[r][c]
-
-                if val == ".":
-                    continue
 
                 box_index = (r // 3) * 3 + (c // 3)
 
+                if val == ".":
+                    continue
+                
                 if val in rows[r]:
                     return False
-
                 if val in cols[c]:
                     return False
-                
                 if val in boxes[box_index]:
                     return False
-            
+                
                 rows[r].add(val)
                 cols[c].add(val)
                 boxes[box_index].add(val)
-            
+        
         return True
+
+        # rows = [set() for _ in range(9)]
+        # cols = [set() for _ in range(9)]
+        # boxes = [set() for _ in range(9)]
+
+        # for r in range(9):
+        #     for c in range(9):
+                
+        #         val = board[r][c]
+
+        #         if val == ".":
+        #             continue
+
+        #         box_index = (r // 3) * 3 + (c // 3)
+
+        #         if val in rows[r]:
+        #             return False
+
+        #         if val in cols[c]:
+        #             return False
+                
+        #         if val in boxes[box_index]:
+        #             return False
+            
+        #         rows[r].add(val)
+        #         cols[c].add(val)
+        #         boxes[box_index].add(val)
+            
+        # return True
         
         # rows = [set() for _ in range(9)]
         # cols = [set() for _ in range(9)]
