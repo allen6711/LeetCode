4bda042728fed0f4695e7e8ed09ab85890f2bd01
allen6711 <allen2990167@gmail.com>
2025-11-20 00:50:15 -0800

    Time: 603 ms (39.89%), Space: 21.2 MB (15.4%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,72 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        results = []
+        nums.sort()
+
+        for i in range(0, n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            self.findTwoSum(nums, i + 1, n - 1, -nums[i], results)
+        
+        return results
+    
+    def findTwoSum(self, nums, left, right, target, results):
+        while left < right:
+            if nums[left] + nums[right] == target:
+                results.append((-target, nums[left], nums[right]))
+                left += 1
+                right -= 1
+                while left < right and nums[left] == nums[left - 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right + 1]:
+                    right -= 1
+            
+            elif nums[left] + nums[right] < target:
+                left += 1
+            
+            else:
+                right -= 1
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # nums.sort()
+        # n = len(nums)
+        # triplets = []
+        # for i in range(n - 2):
+        #     # smallest > 0
+        #     if nums[i] > 0:
+        #         break
+        #     # duplicate pass
+        #     if i > 0 and nums[i] == nums[i - 1]:
+        #         continue
+        #     left, right = i + 1, n - 1
+        #     while left < right:
+        #         current_sum = nums[i] + nums[left] + nums[right]
+        #         if current_sum < 0:
+        #             left += 1
+        #         elif current_sum > 0:
+        #             right -= 1
+        #         else:
+        #             triplets.append([nums[i], nums[left], nums[right]])
+        #             left, right = left + 1, right - 1
+        #             while left < right and nums[left] == nums[left - 1]:
+        #                 left += 1
+        #             while left < right and nums[right] == nums[right + 1]:
+        #                 right -= 1
+        
+        # return triplets
