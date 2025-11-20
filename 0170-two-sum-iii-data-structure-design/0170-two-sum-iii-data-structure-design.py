88529ffade66cb35bc83b439d536b94883899daa
allen6711 <allen2990167@gmail.com>
2025-11-19 22:41:37 -0800

    Time: 239 ms (37.45%), Space: 23.2 MB (22.27%) - LeetHub

--- 0170-two-sum-iii-data-structure-design/0170-two-sum-iii-data-structure-design.py
+++ 0170-two-sum-iii-data-structure-design/0170-two-sum-iii-data-structure-design.py
@@ -2,7 +2,7 @@ class TwoSum:
 
     def __init__(self):
         self.hash = {}
-
+    
     def add(self, number: int) -> None:
         self.hash[number] = self.hash.get(number, 0) + 1
 
@@ -10,12 +10,44 @@ class TwoSum:
         for num in self.hash:
             if num * 2 == value and self.hash.get(num, 0) >= 2:
                 return True
-
+            
             target = value - num
-            if target != num and self.hash.get(target, 0) > 0:
+            if num != target and self.hash.get(target, 0) > 0:
                 return True
         
         return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def __init__(self):
+    #     self.hash = {}
+
+    # def add(self, number: int) -> None:
+    #     self.hash[number] = self.hash.get(number, 0) + 1
+
+    # def find(self, value: int) -> bool:
+    #     for num in self.hash:
+    #         if num * 2 == value and self.hash.get(num, 0) >= 2:
+    #             return True
+
+    #         target = value - num
+    #         if target != num and self.hash.get(target, 0) > 0:
+    #             return True
+        
+    #     return False
 
 
 # Your TwoSum object will be instantiated and called as such:
