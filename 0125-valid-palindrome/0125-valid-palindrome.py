014c1084890793efbc58df1e32c90d0293435ee3
allen6711 <allen2990167@gmail.com>
2025-11-22 15:25:44 -0800

    Time: 11 ms (36%), Space: 18 MB (68.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,6 +5,34 @@ class Solution:
         while left < right:
             while left < right and not s[left].isalpha() and not s[left].isdigit():
                 left += 1
+            while left < right and not s[right].isalpha() and not s[right].isdigit():
+                right -= 1
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            while left < right and not s[left].isalpha() and not s[left].isdigit():
+                left += 1
             
             while left < right and not s[right].isalpha() and not s[right].isdigit():
                 right -= 1
