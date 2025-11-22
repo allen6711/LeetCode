509f987a5f6777c6b33a05d9750cc05fcb57a322
allen6711 <allen2990167@gmail.com>
2025-11-22 15:29:24 -0800

    Time: 11 ms (36%), Space: 18.1 MB (55.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -15,35 +15,22 @@ class Solution:
         
         return True
 
+        # left, right = 0, len(s) - 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-        left, right = 0, len(s) - 1
-
-        while left < right:
-            while left < right and not s[left].isalpha() and not s[left].isdigit():
-                left += 1
+        # while left < right:
+        #     while left < right and not s[left].isalpha() and not s[left].isdigit():
+        #         left += 1
             
-            while left < right and not s[right].isalpha() and not s[right].isdigit():
-                right -= 1
+        #     while left < right and not s[right].isalpha() and not s[right].isdigit():
+        #         right -= 1
             
-            if left < right and s[left].lower() != s[right].lower():
-                return False
+        #     if left < right and s[left].lower() != s[right].lower():
+        #         return False
             
-            left += 1
-            right -= 1
+        #     left += 1
+        #     right -= 1
 
-        return True
+        # return True
 
 
         # start, end = 0, len(s) - 1
