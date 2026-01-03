4ecd311d13de2db07098246c8fb2dc885806229c
allen6711 <allen2990167@gmail.com>
2026-01-03 11:22:21 -0800

    Time: 0 ms (100%), Space: 17.1 MB (99.84%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,30 +1,70 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
+        row = [set() for _ in range(9)]
+        col = [set() for _ in range(9)]
         boxes = [set() for _ in range(9)]
 
         for r in range(9):
             for c in range(9):
                 val = board[r][c]
-
-                box_index = (r // 3) * 3 + (c // 3)
+                box_index = (r // 3) * 3 + c // 3
 
                 if val == ".":
                     continue
-                
-                if val in rows[r]:
+
+                if val in row[r]:
                     return False
-                if val in cols[c]:
+                if val in col[c]:
                     return False
                 if val in boxes[box_index]:
                     return False
                 
-                rows[r].add(val)
-                cols[c].add(val)
+                row[r].add(val)
+                col[c].add(val)
                 boxes[box_index].add(val)
         
         return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # rows = [set() for _ in range(9)]
+        # cols = [set() for _ in range(9)]
+        # boxes = [set() for _ in range(9)]
+
+        # for r in range(9):
+        #     for c in range(9):
+        #         val = board[r][c]
+
+        #         box_index = (r // 3) * 3 + (c // 3)
+
+        #         if val == ".":
+        #             continue
+                
+        #         if val in rows[r]:
+        #             return False
+        #         if val in cols[c]:
+        #             return False
+        #         if val in boxes[box_index]:
+        #             return False
+                
+        #         rows[r].add(val)
+        #         cols[c].add(val)
+        #         boxes[box_index].add(val)
+        
+        # return True
 
         # rows = [set() for _ in range(9)]
         # cols = [set() for _ in range(9)]
