fd11b49ba22bcb83077321906c8cb0bd6e113d1b
allen6711 <allen2990167@gmail.com>
2026-01-06 01:40:07 -0800

    Time: 17 ms (41.23%), Space: 23.9 MB (9.27%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,30 +2,30 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         base = ord('a')
         word_dict = defaultdict(list)
+
         for char in strs:
             cnt = [0] * 26
 
             for ch in char:
                 cnt[ord(ch) - base] += 1
-            
+
             key = tuple(cnt)
             word_dict[key].append(char)
         
         return list(word_dict.values())
 
+        # base = ord('a')
+        # word_dict = defaultdict(list)
+        # for char in strs:
+        #     cnt = [0] * 26
 
-
-
-
-
-
-
-
-
-
-
-
-
+        #     for ch in char:
+        #         cnt[ord(ch) - base] += 1
+            
+        #     key = tuple(cnt)
+        #     word_dict[key].append(char)
+        
+        # return list(word_dict.values())
 
 
         # base = ord('a')
