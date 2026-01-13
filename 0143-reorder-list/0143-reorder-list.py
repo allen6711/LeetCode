ce926861aa2704eb6cb0d3592dbbaab1029aafa0
allen6711 <allen2990167@gmail.com>
2026-01-13 14:18:19 -0800

    Time: 8 ms (9.39%), Space: 28.1 MB (6.18%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,12 +8,16 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        if not head or not head.next:
+            return
+        
+        # Find the middle
         slow, fast = head, head
-
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
+
+        # Reverse Secound half
         prev = None
         cur = slow.next
         slow.next = None
@@ -22,18 +26,50 @@ class Solution:
             cur.next = prev
             prev = cur
             cur = next_node
-        second = prev
-        first = head
 
+        second = prev  # Second head
+        first = head   # First head
+        
+        # Merge two halves
         while second:
             temp1 = first.next
             temp2 = second.next
-
             first.next = second
             second.next = temp1
-
             first = temp1
             second = temp2
-        
 
+
+        # if not head or not head.next:
+        #     return 
+
+        # # Find middle (slow will be at mid)
+        # slow, fast = head, head
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+        
+        # # Reverse second half
+        # prev = None
+        # cur = slow.next
+        # slow.next = None  # cut the list into two halves
+
+        # while cur:
+        #     next_node = cur.next
+        #     cur.next = prev
+        #     prev = cur
+        #     cur = next_node
+        # second = prev  # head of reversed second half
+        # first = head
+
+        # # Merge two halves
+        # while second:
+        #     temp1 = first.next
+        #     temp2 = second.next
+
+        #     first.next = second
+        #     second.next = temp1
+
+        #     first = temp1
+        #     second = temp2
         
