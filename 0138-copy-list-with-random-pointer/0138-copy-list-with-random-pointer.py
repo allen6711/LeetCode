79b8cf0d43a3678cb7e4709a88a1ada11dcd811f
allen6711 <allen2990167@gmail.com>
2026-01-13 16:25:21 -0800

    Time: 47 ms (20.79%), Space: 20.1 MB (12.01%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -11,7 +11,8 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
-        # 1. Interleave copied nodes with original nodes
+        
+        # 1. Clone and interleave
         cur = head
         while cur:
             next_node = cur.next
@@ -19,26 +20,90 @@ class Solution:
             cur.next = copy
             copy.next = next_node
             cur = next_node
-        # 2. Assign random pointers for the copied nodes
+        # 2. Assign random for clone nodes
         cur = head
         while cur:
             copy = cur.next
             copy.random = cur.random.next if cur.random else None
             cur = copy.next
-        # 3. Separate the interleaved list into original and copied lists
+        # 3. Split into two lists
         dummy = Node(0)
         copy_tail = dummy
         cur = head
         while cur:
-            # A'
             copy = cur.next
-            # B
             next_node = copy.next
 
             copy_tail.next = copy
             copy_tail = copy
-            # A to B
+            
             cur.next = next_node
             cur = next_node
         
         return dummy.next
+        
+        # if not head:
+        #     return None
+        # # 1. Clone nodes and interleave
+        # cur = head
+        # while cur:
+        #     next_node = cur.next
+        #     copy = Node(cur.val)
+        #     cur.next = copy
+        #     copy.next = next_node
+        #     cur = next_node
+        # # 2. Assign random for clones
+        # cur = head
+        # while cur:
+        #     copy = cur.next
+        #     copy.random = cur.random.next if cur.random else None
+        #     cur = copy.next
+        # # 3. Split into two lists
+        # dummy = Node(0)
+        # copy_tail = dummy
+        # cur = head
+        # while cur:
+        #     copy = cur.next
+        #     next_node = copy.next
+
+        #     copy_tail.next = copy
+        #     copy_tail = copy
+
+        #     cur.next = next_node
+        #     cur = next_node
+        
+        # return dummy.next
+
+        # if not head:
+        #     return None
+        # # 1. Interleave copied nodes with original nodes
+        # cur = head
+        # while cur:
+        #     next_node = cur.next
+        #     copy = Node(cur.val)
+        #     cur.next = copy
+        #     copy.next = next_node
+        #     cur = next_node
+        # # 2. Assign random pointers for the copied nodes
+        # cur = head
+        # while cur:
+        #     copy = cur.next
+        #     copy.random = cur.random.next if cur.random else None
+        #     cur = copy.next
+        # # 3. Separate the interleaved list into original and copied lists
+        # dummy = Node(0)
+        # copy_tail = dummy
+        # cur = head
+        # while cur:
+        #     # A'
+        #     copy = cur.next
+        #     # B
+        #     next_node = copy.next
+
+        #     copy_tail.next = copy
+        #     copy_tail = copy
+        #     # A to B
+        #     cur.next = next_node
+        #     cur = next_node
+        
+        # return dummy.next
