90667582dbc8d92c5e23902f3388acd746c92b86
allen6711 <allen2990167@gmail.com>
2026-01-16 15:40:32 -0800

    Time: 20 ms (87.01%), Space: 33.6 MB (26.95%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,17 +1,33 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        slow = nums[0]
-        fast = nums[0]
-        
+        # Floyd Cycle Detection
+        slow = 0
+        fast = 0
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
             if slow == fast:
                 break
         
-        finder = nums[0]
-        while finder != slow:
-            finder = nums[finder]
+        slow = 0
+        while slow != fast:
             slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
+
+        # slow = nums[0]
+        # fast = nums[0]
         
-        return finder
+        # while True:
+        #     slow = nums[slow]
+        #     fast = nums[nums[fast]]
+        #     if slow == fast:
+        #         break
+        
+        # finder = nums[0]
+        # while finder != slow:
+        #     finder = nums[finder]
+        #     slow = nums[slow]
+        
+        # return finder
