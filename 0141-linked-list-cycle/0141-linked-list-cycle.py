0867fd3c59d824c3520e42dc65793072e014a76f
allen6711 <allen2990167@gmail.com>
2026-01-19 15:55:20 -0800

    Time: 49 ms (54.84%), Space: 22.7 MB (8.07%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -15,6 +15,22 @@ class Solution:
         
         return False
 
+
+
+
+
+
+
+
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        
+        return False
+
         # slow, fast = head, head
 
         # while fast and fast.next:
