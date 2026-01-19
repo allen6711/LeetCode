96ce0985d4206be9f93b6ce81e2f320e0dce69d1
allen6711 <allen2990167@gmail.com>
2026-01-18 23:00:54 -0800

    Time: 0 ms (100%), Space: 20.3 MB (13.78%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -8,27 +8,27 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         # DFS
-        # if root is None:
-        #     return 0
-        
-        # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-        
-        # BFS
-        from collections import deque
         if root is None:
             return 0
-        q = deque([root])
-        depth = 0
-        while q:
-            level_size = len(q)
-            for _ in range(level_size):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            depth += 1
-        return depth
+        
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
+        # BFS
+        # from collections import deque
+        # if root is None:
+        #     return 0
+        # q = deque([root])
+        # depth = 0
+        # while q:
+        #     level_size = len(q)
+        #     for _ in range(level_size):
+        #         node = q.popleft()
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #     depth += 1
+        # return depth
 
         # Divide and Conquer
         # if root is None:
