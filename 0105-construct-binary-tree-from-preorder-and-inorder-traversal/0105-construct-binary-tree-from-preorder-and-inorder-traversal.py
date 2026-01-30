72ca37f7077037018acc602f5b79268dc0b2dd3f
allen6711 <allen2990167@gmail.com>
2026-01-30 15:33:00 -0800

    Time: 4 ms (67.28%), Space: 21.6 MB (49.98%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,30 +6,54 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        # value -> index in inorder
+        # Store value: index
         index = {}
-        for i, v in enumerate(inorder):
-            index[v] = i
+        for i, val in enumerate(inorder):
+            index[val] = i
         
         n = len(preorder)
-        
+
         def dfs(preL, preR, inL, inR):
-            # build subtree from:
-            # preorder[preL...preR], inorder[inL, inR]
-            # interval is empty, same as inL > inR
             if preL > preR:
                 return None
 
             root_val = preorder[preL]
             root = TreeNode(root_val)
-            k = index[root_val]      # root position in inorder
+            k = index[root_val]
             left_size = k - inL
 
             root.left = dfs(preL + 1, preL + left_size, inL, k - 1)
             root.right = dfs(preL + left_size + 1, preR, k + 1, inR)
 
+
             return root
 
         return dfs(0, n - 1, 0, n - 1)
+
+        # value -> index in inorder
+        # index = {}
+        # for i, v in enumerate(inorder):
+        #     index[v] = i
+        
+        # n = len(preorder)
+        
+        # def dfs(preL, preR, inL, inR):
+        #     # build subtree from:
+        #     # preorder[preL...preR], inorder[inL, inR]
+        #     # interval is empty, same as inL > inR
+        #     if preL > preR:
+        #         return None
+
+        #     root_val = preorder[preL]
+        #     root = TreeNode(root_val)
+        #     k = index[root_val]      # root position in inorder
+        #     left_size = k - inL
+
+        #     root.left = dfs(preL + 1, preL + left_size, inL, k - 1)
+        #     root.right = dfs(preL + left_size + 1, preR, k + 1, inR)
+
+        #     return root
+
+        # return dfs(0, n - 1, 0, n - 1)
 
 
