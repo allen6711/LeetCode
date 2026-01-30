5767e73b7d61c37221ac596490b77395dc0a2011
allen6711 <allen2990167@gmail.com>
2026-01-30 00:22:58 -0800

    Time: 0 ms (100%), Space: 19.5 MB (14.53%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -7,34 +7,42 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         # DFS
-        if p is None and q is None:
-            return True
-        if p is None or q is None:
-            return False
-        if p.val != q.val:
-            return False
+        # if p is None and q is None:
+        #     return True
+        # if p is None or q is None:
+        #     return False
+        # if p.val != q.val:
+        #     return False
         
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        # return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
-
-
-
-
-
-
-
-
-
+        # BFS
+        from collections import deque
+        
+        queue = deque([(p, q)])
+        while queue:
+            node1, node2 = queue.popleft()
+            if node1 is None and node2 is None:
+                continue
+            if node1 is None or node2 is None:
+                return False
+            if node1.val != node2.val:
+                return False
+                
+            queue.append((node1.left, node2.left))
+            queue.append((node1.right, node2.right))
+        
+        return True
 
         # DFS
-        if p is None and q is None:
-            return True
-        if p is None or q is None:
-            return False
-        if p.val != q.val:
-            return False
+        # if p is None and q is None:
+        #     return True
+        # if p is None or q is None:
+        #     return False
+        # if p.val != q.val:
+        #     return False
 
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        # return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
         # BFS
         from collections import deque
