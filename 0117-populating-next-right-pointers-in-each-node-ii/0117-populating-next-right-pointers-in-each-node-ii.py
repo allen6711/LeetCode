2c0165cb3116976a9e3d17266d0d7ee1f8e01fbf
allen6711 <allen2990167@gmail.com>
2026-01-31 00:18:48 -0800

    Time: 50 ms (44.85%), Space: 20.5 MB (13.56%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -11,9 +11,10 @@ class Node:
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
         # BFS
+        from collections import deque
         if not root:
             return None
-        from collections import deque
+        
         q = deque([root])
         while q:
             size = len(q)
@@ -23,10 +24,33 @@ class Solution:
                 if prev:
                     prev.next = node
                 prev = node
+
                 if node.left:
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
+            
             prev.next = None
         
         return root
+
+        # BFS
+        # if not root:
+        #     return None
+        # from collections import deque
+        # q = deque([root])
+        # while q:
+        #     size = len(q)
+        #     prev = None
+        #     for _ in range(size):
+        #         node = q.popleft()
+        #         if prev:
+        #             prev.next = node
+        #         prev = node
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #     prev.next = None
+        
+        # return root
