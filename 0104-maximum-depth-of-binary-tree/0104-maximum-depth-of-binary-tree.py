c2db7d26fa50c405f7d4a2a63f7482fb0e0257aa
allen6711 <allen2990167@gmail.com>
2026-02-01 11:36:23 -0800

    Time: 0 ms (100%), Space: 20.5 MB (12.65%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -8,13 +8,14 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         # DFS
-        if root is None:
-            return 0
+        # if not root:
+        #     return 0
         
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
         # BFS
         from collections import deque
-        if root is None:
+        if not root:
             return 0
         q = deque([root])
         depth = 0
@@ -29,6 +30,38 @@ class Solution:
             depth += 1
         
         return depth
+
+
+
+
+
+
+
+
+
+
+        # # DFS
+        # if root is None:
+        #     return 0
+        
+        # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        # # BFS
+        # from collections import deque
+        # if root is None:
+        #     return 0
+        # q = deque([root])
+        # depth = 0
+        # while q:
+        #     size = len(q)
+        #     for _ in range(size):
+        #         node = q.popleft()
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #     depth += 1
+        
+        # return depth
 
         # DFS
         # if root is None:
