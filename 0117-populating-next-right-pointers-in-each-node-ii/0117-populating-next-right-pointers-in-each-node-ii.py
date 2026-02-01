148c16f39aeea3e6f4cad106db31eaab64f7de6a
allen6711 <allen2990167@gmail.com>
2026-01-31 16:45:53 -0800

    Time: 47 ms (62.85%), Space: 20.5 MB (14.22%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -10,28 +10,70 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        # BFS
-        from collections import deque
+        # dummy + tail
         if not root:
             return None
-        
-        q = deque([root])
-        while q:
-            size = len(q)
-            prev = None
-            for _ in range(size):
-                node = q.popleft()
-                if prev:
-                    prev.next = node
-                prev = node
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            
-            prev.next = None
+        level_start = root
+
+        while level_start:
+            dummy = Node(0)
+            tail = dummy
+            cur = level_start
+            while cur:
+                if cur.left:
+                    tail.next = cur.left
+                    tail = tail.next
+                if cur.right:
+                    tail.next = cur.right
+                    tail = tail.next
+                cur = cur.next
+            level_start = dummy.next
         
         return root
+
+
+        # # dummy + tail
+        # if not root:
+        #     return None
+        
+        # level_start = root
+        # while level_start:
+        #     dummy = Node(0)
+        #     tail = dummy
+        #     cur = level_start
+        #     while cur:
+        #         if cur.left:
+        #             tail.next = cur.left
+        #             tail = tail.next
+        #         if cur.right:
+        #             tail.next = cur.right
+        #             tail = tail.next
+        #         cur = cur.next
+        #     level_start = dummy.next
+        
+        # return root
+        # BFS
+        # from collections import deque
+        # if not root:
+        #     return None
+        
+        # q = deque([root])
+        # while q:
+        #     size = len(q)
+        #     prev = None
+        #     for _ in range(size):
+        #         node = q.popleft()
+        #         if prev:
+        #             prev.next = node
+        #         prev = node
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+            
+        #     prev.next = None
+        
+        # return root
 
         # BFS
         # if not root:
