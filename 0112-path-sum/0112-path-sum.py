9c1a92a6c1318412d50ad866c16df6022336b194
allen6711 <allen2990167@gmail.com>
2026-02-01 16:29:54 -0800

    Time: 3 ms (30.18%), Space: 20.3 MB (29.79%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -13,4 +13,25 @@ class Solution:
         
         new_target = targetSum - root.val
 
+        return self.hasPathSum(root.left, new_target) or self.hasPathSum(root.right, new_target)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return (root.val == targetSum)
+        
+        new_target = targetSum - root.val
+
         return self.hasPathSum(root.left, new_target) or self.hasPathSum(root.right, new_target) 
