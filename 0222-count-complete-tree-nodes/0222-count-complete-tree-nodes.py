f8acc2f6bb76e745606f3ccdb4caba1d63f2ac90
allen6711 <allen2990167@gmail.com>
2026-02-01 16:53:39 -0800

    Time: 0 ms (100%), Space: 24 MB (10.93%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -6,31 +6,60 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-
         def left_height(node):
             h = 0
             while node:
                 h += 1
                 node = node.left
             return h
+        
         def right_height(node):
             h = 0
             while node:
                 h += 1
                 node = node.right
+            return h
 
-        
         def dfs(node):
             if not node:
                 return 0
             
-            hl = left_height(node.left)
-            hr = right_height(node.right)
-            if hl == hr:
-                return 2 ** hl - 1
+            lh = left_height(node)
+            rh = right_height(node)
+
+            if lh == rh:
+                return 2 ** lh - 1
             
             return 1 + dfs(node.left) + dfs(node.right)
         
         return dfs(root)
+
+
+        # def left_height(node):
+        #     h = 0
+        #     while node:
+        #         h += 1
+        #         node = node.left
+        #     return h
+        # def right_height(node):
+        #     h = 0
+        #     while node:
+        #         h += 1
+        #         node = node.right
+        #     return h
+
+        
+        # def dfs(node):
+        #     if not node:
+        #         return 0
+            
+        #     hl = left_height(node.left)
+        #     hr = right_height(node.right)
+        #     if hl == hr:
+        #         return 2 ** hl - 1
+            
+        #     return 1 + dfs(node.left) + dfs(node.right)
+        
+        # return dfs(root)
 
         
