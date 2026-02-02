4081a0a1aeea16bf1432b71b7fec742b1e4e0cb6
allen6711 <allen2990167@gmail.com>
2026-02-01 16:36:09 -0800

    Time: 0 ms (100%), Space: 19.4 MB (34.45%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -6,6 +6,7 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        
         def dfs(node, cur):
             if not node:
                 return 0
@@ -17,6 +18,20 @@ class Solution:
             return dfs(node.left, cur) + dfs(node.right, cur)
         
         return dfs(root, 0)
+
+
+
+        # def dfs(node, cur):
+        #     if not node:
+        #         return 0
+            
+        #     cur = cur * 10 + node.val
+        #     if not node.left and not node.right:
+        #         return cur
+            
+        #     return dfs(node.left, cur) + dfs(node.right, cur)
+        
+        # return dfs(root, 0)
 
         # def dfs(node, cur):
         #     if not node:
