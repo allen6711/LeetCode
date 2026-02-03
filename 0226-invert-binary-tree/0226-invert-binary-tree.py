bb48a878b52d647ebb448e232d5f10561cca78ce
allen6711 <allen2990167@gmail.com>
2026-02-02 18:57:22 -0800

    Time: 0 ms (100%), Space: 19.1 MB (49.08%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -6,29 +6,47 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        # DFS
         if not root:
             return None
         root.left, root.right = root.right, root.left
+
         self.invertTree(root.left)
         self.invertTree(root.right)
-        
+
         return root
 
-        # BFS
-        from collections import deque
-        if not root:
-            return None
-        q = deque([root])
-        while q:
-            node = q.popleft()
-            node.left, node.right = node.right, node.left
-            if node.left:    # None doesn't have .left and .right
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
+
+
+
+
+
+
+
+
+
+        # # DFS
+        # if not root:
+        #     return None
+        # root.left, root.right = root.right, root.left
+        # self.invertTree(root.left)
+        # self.invertTree(root.right)
         
-        return root
+        # return root
+
+        # # BFS
+        # from collections import deque
+        # if not root:
+        #     return None
+        # q = deque([root])
+        # while q:
+        #     node = q.popleft()
+        #     node.left, node.right = node.right, node.left
+        #     if node.left:    # None doesn't have .left and .right
+        #         q.append(node.left)
+        #     if node.right:
+        #         q.append(node.right)
+        
+        # return root
 
 
         # # DFS
