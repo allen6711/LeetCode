98f958cc4abc502b0b45110ce0fc20f51a494acb
allen6711 <allen2990167@gmail.com>
2026-02-02 18:37:15 -0800

    Time: 0 ms (100%), Space: 19.4 MB (36.94%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -7,14 +7,14 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         # DFS
-        # if p is None and q is None:
-        #     return True
-        # if p is None or q is None:
-        #     return False
-        # if p.val != q.val:
-        #     return False
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val != q.val:
+            return False
         
-        # return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         # BFS
         from collections import deque
         pq = deque([(p, q)])
