1bb833553d532a79c5db77ac55e506fdad3a79d3
allen6711 <allen2990167@gmail.com>
2026-02-04 00:05:48 -0800

    Time: 4 ms (10.8%), Space: 21.1 MB (12.49%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -18,10 +18,16 @@ class Solution:
         return dfs(root, float('-inf'), float('inf'))
 
 
-
-
-
-
+        # def dfs(node, low, high):
+        #     if not node:
+        #         return True
+            
+        #     if not (low < node.val < high):
+        #         return False
+            
+        #     return dfs(node.left, low, node.val) and dfs(node.right, node.val, high)
+        
+        # return dfs(root, float('-inf'), float('inf'))
 
 
     #     isBST, _, _ = self.helper(root)
