dfdc7cf0989a4a55307a586a6407d838952f3667
allen6711 <allen2990167@gmail.com>
2026-02-05 23:27:18 -0800

    Time: 0 ms (100%), Space: 19.5 MB (40.72%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -4,11 +4,7 @@ class Solution:
             return False
         
         m, n = len(matrix), len(matrix[0])
-        length = m * n
-        start, end = 0, length - 1
-        # [0, 1, 2, 3]
-        # [4, 5, 6, 7]
-        # [8, 9, 10, 11]
+        start, end = 0, m * n - 1
 
         while start + 1 < end:
             mid = (start + end) // 2
@@ -18,14 +14,15 @@ class Solution:
             
             else:
                 end = mid
-        
-        if matrix[end // n][end % n] == target:
-            return True
-        
+            
         if matrix[start // n][start % n] == target:
             return True
-        
+            
+        if matrix[end // n][end % n] == target:
+            return True
+            
         return False
+    
 
 
 
@@ -41,18 +38,33 @@ class Solution:
 
 
 
+        # if not matrix or not matrix[0]:
+        #     return False
+        
+        # m, n = len(matrix), len(matrix[0])
+        # length = m * n
+        # start, end = 0, length - 1
+        # # [0, 1, 2, 3]
+        # # [4, 5, 6, 7]
+        # # [8, 9, 10, 11]
 
+        # while start + 1 < end:
+        #     mid = (start + end) // 2
 
-
-
-
-
-
-
-
-
-
-
+        #     if matrix[mid // n][mid % n] <= target:
+        #         start = mid
+            
+        #     else:
+        #         end = mid
+        
+        # if matrix[end // n][end % n] == target:
+        #     return True
+        
+        # if matrix[start // n][start % n] == target:
+        #     return True
+        
+        # return False
+        
         # if not matrix or not matrix[0]:
         #     return False
         
