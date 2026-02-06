340bb8a55b5f9f7a5582ae74a8d5323e83b79c32
allen6711 <allen2990167@gmail.com>
2026-02-05 22:59:20 -0800

    Time: 0 ms (100%), Space: 20.6 MB (28.73%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -2,46 +2,77 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         if not nums:
             return [-1, -1]
-        
-        first_position, last_position = -1, -1
-        
-        # first position:
-        start, end = 0, len(nums) - 1
-        
-        while start + 1 < end:
-            mid = (start + end) // 2
-
+        left, right = 0, len(nums)
+        while left < right:
+            mid = (left + right) // 2
             if nums[mid] >= target:
-                end = mid
-            
+                right = mid
             else:
-                start = mid
-
-        if nums[start] == target:
-            first_position = start
+                left = mid + 1
         
-        elif nums[end] == target:
-            first_position = end
+        l = left
 
-        # end position:
-        start, end = 0, len(nums) - 1
-
-        while start + 1 < end:
-            mid = (start + end) // 2
-
+        left, right = 0, len(nums)
+        while left < right:
+            mid = (left + right) // 2
             if nums[mid] <= target:
-                start = mid
-            
+                left = mid + 1
             else:
-                end = mid
+                right = mid
         
-        if nums[end] == target:
-            last_position = end
+        r = left - 1
 
-        elif nums[start] == target:
-            last_position = start
+        # nums = [1, 3, 5], target = 10
+        # left = 3
+        if l == len(nums) or nums[l] != target:
+            return [-1, -1]
+        
+        return [l, r]
 
-        return [first_position, last_position]
+
+
+
+
+        
+        # first_position, last_position = -1, -1
+        
+        # # first position:
+        # start, end = 0, len(nums) - 1
+        
+        # while start + 1 < end:
+        #     mid = (start + end) // 2
+
+        #     if nums[mid] >= target:
+        #         end = mid
+            
+        #     else:
+        #         start = mid
+
+        # if nums[start] == target:
+        #     first_position = start
+        
+        # elif nums[end] == target:
+        #     first_position = end
+
+        # # end position:
+        # start, end = 0, len(nums) - 1
+
+        # while start + 1 < end:
+        #     mid = (start + end) // 2
+
+        #     if nums[mid] <= target:
+        #         start = mid
+            
+        #     else:
+        #         end = mid
+        
+        # if nums[end] == target:
+        #     last_position = end
+
+        # elif nums[start] == target:
+        #     last_position = start
+
+        # return [first_position, last_position]
         
         
 
