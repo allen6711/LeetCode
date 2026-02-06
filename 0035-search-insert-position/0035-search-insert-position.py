4a657c6fc8b5686bb66b7ee56bdc9bfd5c8f296b
allen6711 <allen2990167@gmail.com>
2026-02-06 15:57:22 -0800

    Time: 0 ms (100%), Space: 20 MB (25.78%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -7,35 +7,41 @@ class Solution:
                 right = mid
             else:
                 left = mid
-            
+        
         if nums[left] == target:
             return left
         if nums[right] == target:
             return right
         
-        # edge cases        
-        if target <= nums[0]:
-            return 0
         if target > nums[-1]:
             return len(nums)
+        elif target < nums[0]:
+            return 0
+        else:
+            return right
+
+
+ 
+        # left, right = 0, len(nums) - 1
+        # while left + 1 < right:
+        #     mid = (left + right) // 2
+        #     if nums[mid] >= target:
+        #         right = mid
+        #     else:
+        #         left = mid
+            
+        # if nums[left] == target:
+        #     return left
+        # if nums[right] == target:
+        #     return right
         
-        return right
+        # # edge cases        
+        # if target <= nums[0]:
+        #     return 0
+        # if target > nums[-1]:
+        #     return len(nums)
         
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # return right
 
         # left, right = 0, len(nums)
         # while left < right:
