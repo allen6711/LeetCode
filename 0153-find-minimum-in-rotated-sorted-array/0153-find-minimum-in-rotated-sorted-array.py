5e6ced63fd658fc2c72adae59e125072e2367202
allen6711 <allen2990167@gmail.com>
2026-02-06 20:55:09 -0800

    Time: 0 ms (100%), Space: 19.6 MB (15.44%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,78 +1,11 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        start, end = 0, len(nums) - 1
-        while start + 1 < end:
-            mid = (start + end) // 2
-            # Check the positions is at the peak's left or right
+        left, right = 0, len(nums) - 1
+        while left + 1 < right:
+            mid = (left + right) // 2
             if nums[mid] <= nums[-1]:
-                end = mid
+                right = mid
             else:
-                start = mid
+                left = mid
         
-        return min(nums[start], nums[end])
-        
-        # if not nums:
-        #     return -1
-
-        # start, end = 0, len(nums) - 1
-
-        # while start + 1 < end:
-        #     mid = (start + end) // 2
-
-        #     if nums[mid] <= nums[-1]:
-        #         end = mid
-            
-        #     else:
-        #         start = mid
-        
-        # # start may be less than end
-        # return min(nums[start], nums[end])
-
-
-        # if not nums:
-        #     return -1
-        
-        # start, end = 0, len(nums) - 1
-
-        # while start + 1 < end:
-        #     mid = (start + end) // 2
-
-        #     if nums[mid] <= nums[-1]:
-        #         end = mid
-
-        #     else:
-        #         start = mid
-        
-        # return min(nums[start], nums[end])
-
-
-
-
-        # if not nums:
-        #     return -1
-        
-        # start, end = 0, len(nums) - 1
-
-        # while start + 1 < end:
-        #     mid = (start + end) // 2
-
-        #     if nums[mid] <= nums[-1]:
-        #         end = mid
-            
-        #     else:
-        #         start = mid
-
-        # return min(nums[start], nums[end])
-
-
-        # left, right = 0, len(nums) - 1
-        # index = -1
-
-        # while left <= right:
-        #     mid = (left + right) // 2
-        #     if nums[mid] <= nums[-1]:
-        #         index = mid
-        #         right = mid - 1
-        #     else:
-        #         left = mid + 1
-        # return nums[index]
+        return min(nums[left], nums[right])
