50efc84cc2b085e822846ac5fed97f44a0cb69db
allen6711 <allen2990167@gmail.com>
2026-02-06 16:30:30 -0800

    Time: 0 ms (100%), Space: 19.5 MB (24.79%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,32 +1,63 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        if not nums:
-            return -1
-        
-        start, end = 0, len(nums) - 1
-
-        while start + 1 < end:
-            mid = (start + end) // 2
-
-            # left side of peak
-            if nums[mid - 1] < nums[mid]:
-                start = mid
-            
-            elif nums[mid] < nums[mid - 1]:
-                end = mid
-            
-            # elif nums[mid - 1] < nums[mid] < nums[mid + 1]:
-            #     return mid
+        left, right = 0, len(nums) - 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if nums[mid] < nums[mid + 1]:
+                left = mid
+            elif nums[mid] > nums[mid + 1]:
+                right = mid
             else:
                 return mid
         
-        if nums[start] > nums[end]:
-            return start
-        
+        # peak at nums[0] or nums[-1]
+        if nums[left] > nums[right]:
+            return left
         else:
-            return end
+            return right
+                    
+        return min(nums[left], nums[right])
 
-        return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if not nums:
+        #     return -1
+        
+        # start, end = 0, len(nums) - 1
+
+        # while start + 1 < end:
+        #     mid = (start + end) // 2
+
+        #     # left side of peak
+        #     if nums[mid - 1] < nums[mid]:
+        #         start = mid
+            
+        #     elif nums[mid] < nums[mid - 1]:
+        #         end = mid
+            
+        #     # elif nums[mid - 1] < nums[mid] < nums[mid + 1]:
+        #     #     return mid
+        #     else:
+        #         return mid
+        
+        # if nums[start] > nums[end]:
+        #     return start
+        
+        # else:
+        #     return end
+
+        # return -1
 
 
 
