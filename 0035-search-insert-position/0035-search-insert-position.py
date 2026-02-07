e4429d9035fca756bdd4df4f81b06daf5d2d3aa6
allen6711 <allen2990167@gmail.com>
2026-02-07 10:17:03 -0800

    Time: 0 ms (100%), Space: 20.1 MB (25.78%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,5 +1,8 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+
         left, right = 0, len(nums) - 1
         while left + 1 < right:
             mid = (left + right) // 2
@@ -13,12 +16,34 @@ class Solution:
         if nums[right] == target:
             return right
         
+        # Edge cases
         if target > nums[-1]:
             return len(nums)
-        elif target < nums[0]:
+        if target < nums[0]:
             return 0
-        else:
-            return right
+
+        return right
+
+
+        # left, right = 0, len(nums) - 1
+        # while left + 1 < right:
+        #     mid = (left + right) // 2
+        #     if nums[mid] >= target:
+        #         right = mid
+        #     else:
+        #         left = mid
+        
+        # if nums[left] == target:
+        #     return left
+        # if nums[right] == target:
+        #     return right
+        
+        # if target > nums[-1]:
+        #     return len(nums)
+        # elif target < nums[0]:
+        #     return 0
+        # else:
+        #     return right
 
 
  
