70ad550467339f01cd0be18fa3c03c51bc67ab2f
allen6711 <allen2990167@gmail.com>
2026-02-07 16:31:05 -0800

    Time: 0 ms (100%), Space: 19.7 MB (25.74%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -7,22 +7,64 @@ class Solution:
             m, n = n, m
         
         half = (m + n + 1) // 2
-        lo, hi = 0, m
-        while lo <= hi:
-            i = (lo + hi) // 2
+        low, high = 0, m
+        while low <= high:
+            i = (low  + high) // 2
             j = half - i
 
+            # Consider edge
             A_left = -math.inf if i == 0 else A[i - 1]
             A_right = math.inf if i == m else A[i]
             B_left = -math.inf if j == 0 else B[j - 1]
             B_right = math.inf if j == n else B[j]
-
+            
             if A_left <= B_right and A_right >= B_left:
                 if (m + n) % 2 == 1:
                     return float(max(A_left, B_left))
-                return (max(A_left, B_left) + min(A_right, B_right)) / 2.0
-            
+                else:
+                    return (max(A_left, B_left) + min(A_right, B_right)) / 2.0
+
             if A_left > B_right:
-                hi = i - 1
+                high = i - 1
             else:
-                lo = i + 1
+                low = i + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # A, B = nums1, nums2
+        # m, n = len(A), len(B)
+        # if m > n:
+        #     A, B = B, A
+        #     m, n = n, m
+        
+        # half = (m + n + 1) // 2
+        # lo, hi = 0, m
+        # while lo <= hi:
+        #     i = (lo + hi) // 2
+        #     j = half - i
+
+        #     A_left = -math.inf if i == 0 else A[i - 1]
+        #     A_right = math.inf if i == m else A[i]
+        #     B_left = -math.inf if j == 0 else B[j - 1]
+        #     B_right = math.inf if j == n else B[j]
+
+        #     if A_left <= B_right and A_right >= B_left:
+        #         if (m + n) % 2 == 1:
+        #             return float(max(A_left, B_left))
+        #         return (max(A_left, B_left) + min(A_right, B_right)) / 2.0
+            
+        #     if A_left > B_right:
+        #         hi = i - 1
+        #     else:
+        #         lo = i + 1
