e20ac6aee1ef27c71833581b82fcef87fb02db39
allen6711 <allen2990167@gmail.com>
2026-02-07 20:41:41 -0800

    Time: 7 ms (27.53%), Space: 20.8 MB (23.17%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,16 +1,14 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers) - 1
-
         while left < right:
-            if numbers[left] + numbers[right] == target:
-                return [left + 1, right + 1]
+            if numbers[left] + numbers[right] > target:
+                right -= 1
             elif numbers[left] + numbers[right] < target:
                 left += 1
+            
             else:
-                right -= 1
-        
-        return []
+                return [left + 1, right + 1]
 
 
 
@@ -28,11 +26,23 @@ class Solution:
         left, right = 0, len(numbers) - 1
 
         while left < right:
-            if left < right and numbers[left] + numbers[right] == target:
+            if numbers[left] + numbers[right] == target:
                 return [left + 1, right + 1]
-            if left < right and numbers[left] + numbers[right] > target:
-                right -= 1
-            if left < right and numbers[left] + numbers[right] < target:
+            elif numbers[left] + numbers[right] < target:
                 left += 1
+            else:
+                right -= 1
         
         return []
+
+        # left, right = 0, len(numbers) - 1
+
+        # while left < right:
+        #     if left < right and numbers[left] + numbers[right] == target:
+        #         return [left + 1, right + 1]
+        #     if left < right and numbers[left] + numbers[right] > target:
+        #         right -= 1
+        #     if left < right and numbers[left] + numbers[right] < target:
+        #         left += 1
+        
+        # return []
