21fee42f412e6d48c5bdebf7f9e0ef80075ed509
allen6711 <allen2990167@gmail.com>
2026-02-08 16:17:35 -0800

    Time: 11 ms (84.86%), Space: 19.5 MB (19.52%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,14 +1,43 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        unique_chars = set()
-        max_length = 0
-        left_pointer = 0
-        for right_pointer, char in enumerate(s):
-            while char in unique_chars:
-                unique_chars.remove(s[left_pointer])
-                left_pointer += 1
-                
-            unique_chars.add(s[right_pointer])
-            max_length = max(max_length, right_pointer - left_pointer + 1)
+        last = {}  # char -> last index
+        left = 0
+        best = 0
+
+        for right, char in enumerate(s):
+            if char in last and last[char] >= left:
+                left = last[char] + 1
+            
+            last[char] = right
+            best = max(best, right - left + 1)
         
-        return max_length
+        return best
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # unique_chars = set()
+        # max_length = 0
+        # left_pointer = 0
+        # for right_pointer, char in enumerate(s):
+        #     while char in unique_chars:
+        #         unique_chars.remove(s[left_pointer])
+        #         left_pointer += 1
+                
+        #     unique_chars.add(s[right_pointer])
+        #     max_length = max(max_length, right_pointer - left_pointer + 1)
+        
+        # return max_length
