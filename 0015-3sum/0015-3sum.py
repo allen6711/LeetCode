cd3c2277d7cbb88193355889d75856f1caa57684
allen6711 <allen2990167@gmail.com>
2026-02-10 14:33:45 -0800

    Time: 482 ms (85.21%), Space: 22.3 MB (32.31%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,6 +2,7 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         n = len(nums)
+        i, j = 0, n - 1
         ans = []
 
         for i in range(n - 2):
@@ -9,27 +10,58 @@ class Solution:
                 break
             
             # Pass the same num
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i > 0 and nums[i - 1] == nums[i]:
                 continue
-            
+
             left, right = i + 1, n - 1
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
                 if total < 0:
                     left += 1
-                elif total >0:
+                elif total > 0:
                     right -= 1
                 else:
                     ans.append([nums[i], nums[left], nums[right]])
                     left += 1
                     right -= 1
-
-                    while left < right and nums[left] == nums[left - 1]:
+                    while left < right and nums[left - 1] == nums[left]:
                         left += 1
-                    while left < right and nums[right] == nums[right + 1]:
+                    while left < right and nums[right + 1] == nums[right]:
                         right -= 1
-        
+
         return ans
+
+
+        # nums.sort()
+        # n = len(nums)
+        # ans = []
+
+        # for i in range(n - 2):
+        #     if nums[i] > 0:
+        #         break
+            
+        #     # Pass the same num
+        #     if i > 0 and nums[i] == nums[i - 1]:
+        #         continue
+            
+        #     left, right = i + 1, n - 1
+        #     while left < right:
+        #         total = nums[i] + nums[left] + nums[right]
+        #         if total < 0:
+        #             left += 1
+        #         elif total >0:
+        #             right -= 1
+        #         else:
+        #             ans.append([nums[i], nums[left], nums[right]])
+        #             left += 1
+        #             right -= 1
+
+        #             while left < right and nums[left] == nums[left - 1]:
+        #                 left += 1
+        #             while left < right and nums[right] == nums[right + 1]:
+        #                 right -= 1
+        
+        # return ans
 
         # n = len(nums)
         # results = []
