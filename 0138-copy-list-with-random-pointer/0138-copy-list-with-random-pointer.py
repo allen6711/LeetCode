bcb97105a1c412e893bd03001133c2d8368db608
allen6711 <allen2990167@gmail.com>
2026-02-15 15:19:50 -0800

    Time: 45 ms (57.18%), Space: 20.1 MB (62.21%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -11,6 +11,76 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
+        # Copy node
+        ## A -> B -> C
+        ## A -> A' -> B -> B' -> C -> C'
+        cur = head
+        while cur:
+            copy = Node(cur.val)
+            next_node = cur.next
+            cur.next = copy
+            copy.next = next_node
+            cur = next_node
+        # Copy random
+        ## A -> C, A' -> C'
+        cur = head
+        while cur:
+            copy = cur.next
+            copy.random = cur.random.next if cur.random else None
+            cur = copy.next
+        # Split into 2 lists
+        dummy = Node(0)
+        copy_tail = dummy
+        cur = head
+        while cur:
+            copy = cur.next
+            copy_tail.next = copy
+            copy_tail = copy
+            cur = copy.next
+        
+        return dummy.next
+
+            
+
+
+
+
+
+
+
+
+        # if not head:
+        #     return None
+        # # Clone node
+        # ## x -> y -> z
+        # ##   \> x'\>y'
+        # cur = head
+        # while cur:
+        #     copy = Node(cur.val)
+        #     next_node = cur.next
+        #     cur.next = copy
+        #     copy.next = next_node
+        #     cur = next_node
+        # # Clone random pointer
+        # cur = head
+        # while cur:
+        #     copy = cur.next
+        #     copy.random = cur.random.next if cur.random else None
+        #     cur = copy.next
+        # # Split into 2 lists
+        # dummy = Node(0)
+        # copy_tail = dummy
+        # cur = head
+        # while cur:
+        #     copy = cur.next
+        #     copy_tail.next = copy
+        #     copy_tail = copy
+        #     cur = copy.next
+        
+        # return dummy.next
+
+        if not head:
+            return None
         # Clone nodes
         cur = head
         while cur:
