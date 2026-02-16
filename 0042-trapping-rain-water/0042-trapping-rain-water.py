d975d219fd6d1952aab3fe7cd176fd2a24b65f56
allen6711 <allen2990167@gmail.com>
2026-02-15 23:05:08 -0800

    Time: 7 ms (79.12%), Space: 21.2 MB (34.26%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -21,38 +21,23 @@ class Solution:
         
         return water
 
+        # left, right = 0, len(height) - 1
+        # leftMax, rightMax = 0, 0
+        # water = 0
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        left, right = 0, len(height) - 1
-        leftMax, rightMax = 0, 0
-        water = 0
-
-        while left < right:
-            if height[left] <= height[right]:
-                if height[left] >= leftMax:
-                    leftMax = height[left]
-                else:
-                    water += leftMax - height[left]
-                left += 1
-            else:
-                if height[right] >= rightMax:
-                    rightMax = height[right]
-                else:
-                    water += rightMax - height[right]
-                right -= 1
+        # while left < right:
+        #     if height[left] <= height[right]:
+        #         if height[left] >= leftMax:
+        #             leftMax = height[left]
+        #         else:
+        #             water += leftMax - height[left]
+        #         left += 1
+        #     else:
+        #         if height[right] >= rightMax:
+        #             rightMax = height[right]
+        #         else:
+        #             water += rightMax - height[right]
+        #         right -= 1
         
-        return water
+        # return water
             
