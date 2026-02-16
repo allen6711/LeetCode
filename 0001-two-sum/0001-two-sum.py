f957eb8557509f21d432523349f3f06a9180d6a9
allen6711 <allen2990167@gmail.com>
2026-02-15 21:17:31 -0800

    Time: 1 ms (59.76%), Space: 20.6 MB (14.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,81 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        index = {}
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in index:
-                return [index[diff], i]
-            index[nums[i]] = i
-        
-        return []
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         visited = {}
-
         for i, num in enumerate(nums):
-            if target - num in visited:
-                return [visited[target - num], i]
-            
+            diff = target - num
+            if diff in visited:
+                return [visited[diff], i]
+
             visited[num] = i
         
         return []
-
-        # visited = set()
-        # num_index = defaultdict(int)
-
-        # for i, num in enumerate(nums):
-        #     if target - num in visited:
-        #         return [num_index[target - num], i]
-            
-        #     visited.add(num)
-        #     num_index[num] = i
-        
-        # return False
-
-        # visited = set()
-        # num_index = {}
-
-        # for i, num in enumerate(nums):
-        #     if target - num in visited:
-        #         return [num_index[target - num], i]
-        #     visited.add(num)
-        #     num_index[num] = i
-        
-        # return False
-
-        # num_index = {}
-
-        # for i, num in enumerate(nums):
-        #     dif = target - num
-        #     if dif in num_index:
-        #         return [num_index[dif], i]
-
-        #     num_index[num] = i
-        
-        # return False
-
-        # num_index = {}
-
-        # for i, num in enumerate(nums):
-        #     diff = target - num
-        #     if diff in num_index:
-        #         return [num_index[diff], i]
-
-        #     num_index[num] = i
-        
-        # return False
