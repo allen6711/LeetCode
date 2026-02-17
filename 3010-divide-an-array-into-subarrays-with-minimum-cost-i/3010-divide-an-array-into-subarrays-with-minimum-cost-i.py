6d7b04e675a8026b5206bc97c3e1e5844d4a1a5b
allen6711 <allen2990167@gmail.com>
2026-02-17 11:13:05 -0800

    Time: 0 ms (100%), Space: 19.4 MB (24.67%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -1,18 +1,51 @@
 class Solution:
     def minimumCost(self, nums: List[int]) -> int:
+        # O(nlogn)
+        # sorted_nums = sorted(nums[1:])
+        # return nums[0] + sorted_nums[0] + sorted_nums[1]
+        
+        # O(n)
+        min1 = float('inf')
+        min2 = float('inf')
+        for num in nums[1:]:
+            if num < min1:
+                min2 = min1
+                min1 = num
+            elif num < min2:
+                min2 = num
+        
+        return nums[0] + min1 + min2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # first_cost = nums[0]
         # remaining_sorted = sorted(nums[1:])
 
         # return first_cost + remaining_sorted[0] + remaining_sorted[1]
 
-        min1 = float('inf')
-        min2 = float('inf')
+        # min1 = float('inf')
+        # min2 = float('inf')
         
-        for x in nums[1:]:
-            if x < min1:
-                min2 = min1
-                min1 = x
-            elif x < min2:
-                min2 = x
+        # for x in nums[1:]:
+        #     if x < min1:
+        #         min2 = min1
+        #         min1 = x
+        #     elif x < min2:
+        #         min2 = x
         
-        return nums[0] + min1 + min2
+        # return nums[0] + min1 + min2
