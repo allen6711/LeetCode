64944a1edb5c1e52b8c926b46e4cde2847b2de46
allen6711 <allen2990167@gmail.com>
2026-02-17 22:49:24 -0800

    Time: 1 ms (89.14%), Space: 21.2 MB (64.09%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,29 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # O(n)
-        freq = defaultdict(int)
-        threshold = len(nums) // 2
-        for num in nums:
-            freq[num] += 1
-            if freq[num] > threshold:
-                return num
+        # O(n)
+        # freq = defaultdict(int)
+        # threshold = len(nums) // 2
+        # for num in nums:
+        #     freq[num] += 1
+        #     if freq[num] > threshold:
+        #         return num
         
-        return -1
+        # return -1
+        # O(n)
+        # O(1)
+        count = 0
+        candidate = None
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count = 1
+            elif num == candidate:
+                count += 1
+            else:
+                count -= 1
+        
+        return candidate
 
 
 
