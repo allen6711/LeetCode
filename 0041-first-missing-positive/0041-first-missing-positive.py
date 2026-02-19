2bf220299b19516caf25f319dff33422ee955d1b
allen6711 <allen2990167@gmail.com>
2026-02-18 17:59:14 -0800

    Time: 11 ms (99.45%), Space: 35.2 MB (8.26%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -7,3 +7,19 @@ class Solution:
         while x in num_set:
             x += 1
         return x
+        # O(n)
+        # O(1)
+        n = len(nums)
+        for i in range(n):
+            while 1 <= nums[i] <= n:
+                correct_idx = nums[i] - 1
+                if nums[correct_idx] == nums[i]:
+                    break
+                nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
+        
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        
+        return n + 1
+                
