7c19a6e5854d48a562d62a59b976d08c11aea4b2
allen6711 <allen2990167@gmail.com>
2026-02-18 22:14:11 -0800

    Time: 0 ms (100%), Space: 19.6 MB (8.3%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -1,5 +1,17 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
+        # O(n)
+        # O(n)
+        # n = len(nums)
+        # ans = []
+        # for i in range(n):
+        #     ans[i] = nums[i]
+        # for i in range(n):
+        #     ans[i + n] = nums[i]
+        # return ans
+
+        # O(n)
+        # O(n)
         ans = []
         n = len(nums)
         for i in range(2 * n):
@@ -7,22 +19,6 @@ class Solution:
         
         return ans
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        ans = []
-        for i in range(len(nums)):
-            for i in range(len(nums)):
-                ans.append(nums[i])
-        
-        return ans
+        # O(n)
+        # O(n)
+        # return nums + nums
