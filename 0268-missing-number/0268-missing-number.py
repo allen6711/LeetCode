e7a2db0f6cc23cc009fa6c2d3848045dba22cd8e
allen6711 <allen2990167@gmail.com>
2026-02-18 17:22:50 -0800

    Time: 0 ms (100%), Space: 20.5 MB (56.51%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,8 +1,29 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        num_set = set(nums)
+        # O(nlogn)
+        # O(1)
+        # nums.sort()
+        # for i, num in enumerate(nums):
+        #     if i != num:
+        #         return i
+        # return len(nums)
+        # O(n)
+        # O(n)
+        # num_set = set(nums)
+        # n = len(nums)
+        # for num in range(n + 1):
+        #     if num not in num_set:
+        #         return num
+        # return -1
+        # O(n)
+        # O(1)
+        ## Sum
         n = len(nums)
-        for num in range(n + 1):
-            if num not in num_set:
-                return num
+        expected = n * (n + 1) // 2
+        actual = sum(nums)
+        return expected - actual
+        ## XOR
+        # n = len(nums)
+        
+
      
