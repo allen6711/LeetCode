cad31ebe8c78a084c75bb5be2cbad02dad151717
allen6711 <allen2990167@gmail.com>
2026-02-18 21:56:36 -0800

    Time: 0 ms (100%), Space: 19.4 MB (35.18%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -2,22 +2,22 @@ class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         # O(n^2)
         # O(1)
-        n = len(nums)
-        ans = []
-        for i in range(n):
-            total = 0
-            for j in range(i + 1):
-                total += nums[j]
-            ans.append(total)
-        return ans
+        # n = len(nums)
+        # ans = []
+        # for i in range(n):
+        #     total = 0
+        #     for j in range(i + 1):
+        #         total += nums[j]
+        #     ans.append(total)
+        # return ans
 
         # O(n)
         # O(1)
-        # prefix = 0
-        # n = len(nums)
-        # ans = []
-        # for num in nums:
-        #     prefix += num
-        #     ans.append(prefix)
+        prefix = 0
+        n = len(nums)
+        ans = []
+        for num in nums:
+            prefix += num
+            ans.append(prefix)
         
-        # return ans
+        return ans
