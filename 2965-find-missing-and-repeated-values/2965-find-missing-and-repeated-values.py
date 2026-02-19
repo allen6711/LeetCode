cf2a81a216f659ee89aa7ee77a954a79229b432d
allen6711 <allen2990167@gmail.com>
2026-02-19 15:01:14 -0800

    Time: 7 ms (67.37%), Space: 19.7 MB (35.01%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
@@ -2,17 +2,23 @@ class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         # O(n^2)
         # O(n^2)
-        row_n, col_n = len(grid), len(grid[0])
-        length = row_n * col_n
+        m, n = len(grid), len(grid[0])
+        length = m * n
         freq = [0] * (length + 1)
-
         for row in grid:
             for num in row:
                 freq[num] += 1
-        repeated = missing = -1
+
+        repeated = -1
+        missing = -1
+        
         for num in range(1, length + 1):
             if freq[num] == 2:
                 repeated = num
             elif freq[num] == 0:
                 missing = num
+        
         return [repeated, missing]
+        # O(n^2)
+        # O(1)
+        
