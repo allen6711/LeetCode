6d7318df6d9ebf180e8377eaec3bba0e03c235d6
allen6711 <allen2990167@gmail.com>
2026-02-20 17:45:06 -0800

    Time: 15 ms (99.01%), Space: 19.6 MB (21.57%) - LeetHub

---
+++ 0018-4sum/0018-4sum.py
@@ -0,0 +1,136 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        # O(n^4)
+        # O(m)
+        # n = len(nums)
+        # ans = []
+        # visited = set()
+        
+        # for i in range(n):
+        #     for j in range(i + 1, n):
+        #         for k in range(j + 1, n):
+        #             for l in range(k + 1, n):
+        #                 if nums[i] + nums[j] + nums[k] + nums[l] == target:
+        #                     quad = tuple(sorted([nums[i], nums[j], nums[k], nums[l]]))
+        #                     if quad not in visited:
+        #                         visited.add(quad)
+        #                         ans.append(list(quad))
+        # return ans
+        # O(n^3)
+        # O(1)
+        nums.sort()
+        n = len(nums)
+        ans = []
+        for i in range(n - 3):
+            # Skip duplicate i
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
+            # Prune by min/max possible sums with fixed i
+            min_sum_i = nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3]
+            if min_sum_i > target:
+                break    # i increases -> min_sum_i only grows
+            max_sum_i = nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3]
+            if max_sum_i < target:
+                continue  # even the largest is too small
+            for j in range(i + 1, n - 2):
+                # Skip duplicate j under the same i
+                if j > i + 1 and nums[j - 1] == nums[j]:
+                    continue
+                # Prune by min/max possible sums with fixed i, j
+                min_sum_ij = nums[i] + nums[j] + nums[j + 1] + nums[j + 2]
+                if min_sum_ij > target:
+                    break
+                max_sum_ij = nums[i] + nums[j] + nums[n - 1] + nums[n - 2]
+                if max_sum_ij < target:
+                    continue
+                
+                left, right = j + 1, n - 1
+                need = target - nums[i] - nums[j]
+                while left < right:
+                    two = nums[left] + nums[right]
+                    if two == need:
+                        ans.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif two < need:
+                        left += 1
+                    else:
+                        right -= 1
+        return ans
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        n = len(nums)
+        results = []
+        nums.sort()
+
+        for i in range(0, n - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                left = j + 1
+                right = n - 1
+                while left < right:
+                    if nums[i] + nums[j] + nums[left] + nums[right] == target:
+                        results.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif nums[i] + nums[j] + nums[left] + nums[right] > target:
+                        right -= 1
+                    else:
+                        left += 1
+        return results
+
+        # n = len(nums)
+        # results = []
+        # nums.sort()
+        
+        # # a, b, c, d
+        # # 0, 1, n-2, n-1
+        # # i, i + 1, i + 2, n - 1
+        # # n - 4, n - 3, n - 2, n - 1
+        # for i in range(0, n - 3):
+        #     if i > 0 and nums[i] == nums[i - 1]:
+        #         continue
+        #     for j in range(i + 1, n - 2):
+        #         if j != i + 1 and nums[j] == nums[j - 1]:
+        #             continue
+        #         left = j + 1
+        #         right = n - 1
+        #         while left < right:
+        #             if nums[i] + nums[j] + nums[left] + nums[right] == target:
+        #                 results.append([nums[i], nums[j], nums[left], nums[right]])
+        #                 left += 1
+        #                 right -= 1
+        #                 while left < right and nums[left] == nums[left - 1]:
+        #                     left += 1
+        #                 while left < right and nums[right] == nums[right + 1]:
+        #                     right -= 1
+        #             elif nums[i] + nums[j] + nums[left] + nums[right] > target:
+        #                 right -= 1
+        #             else:
+        #                 left += 1
+        # return results
