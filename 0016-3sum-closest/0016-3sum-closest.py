0cb60d2e5faadabddf68a8dec5f20239dd562037
allen6711 <allen2990167@gmail.com>
2026-02-20 22:13:50 -0800

    Time: 334 ms (82.55%), Space: 19.4 MB (65.6%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -0,0 +1,38 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        # O(n^3)
+        # O(1)
+        # n = len(nums)
+        # best_sum = nums[0] + nums[1] + nums[2]
+        # best_diff = abs(best_sum - target)
+
+        # for i in range(n):
+        #     for j in range(i + 1, n):
+        #         for k in range(j + 1, n):
+        #             total = nums[i] + nums[j] + nums[k]
+        #             diff = abs(total - target)
+        #             if diff < best_diff:
+        #                 best_sum = total
+        #                 best_diff = diff
+        # return best_sum
+        # O(n^2)
+        # O(1)
+        nums.sort()
+        n = len(nums)
+        best_sum = nums[0] + nums[1] + nums[2]
+        best_diff = abs(best_sum - target)
+        for i in range(n - 2):
+            left, right = i + 1, n - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                diff = abs(total - target)
+                if diff <= best_diff:
+                    best_sum = nums[i] + nums[left] + nums[right]
+                    best_diff = diff
+                if total == target:
+                    return target
+                elif total < target:
+                    left += 1
+                else:
+                    right -= 1
+        return best_sum
