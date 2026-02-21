85e028fcba3a0885e82f5348f10969c698274bde
allen6711 <allen2990167@gmail.com>
2026-02-20 21:12:15 -0800

    Time: 15 ms (99.01%), Space: 19.4 MB (46.99%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,54 +1,5 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        # O(n^3)
-        # O(1)
-        nums.sort()
-        n = len(nums)
-        ans = []
-        for i in range(n - 3):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            min_sum_i = nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3]
-            if min_sum_i > target:
-                break
-            max_sum_i = nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3]
-            if max_sum_i < target:
-                continue
-            for j in range(i + 1, n - 2):
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
-                min_sum_ij = nums[i] + nums[j] + nums[j + 1] + nums[j + 2]
-                if min_sum_ij > target:
-                    break
-                max_sum_ij = nums[i] + nums[j] + nums[n - 1] + nums[n - 2]
-                if max_sum_ij < target:
-                    continue
-                left, right = j + 1, n - 1
-                while left < right:
-                    two_sum = nums[left] + nums[right]
-                    if two_sum + nums[i] + nums[j] == target:
-                        ans.append([nums[i], nums[j], nums[left], nums[right]])
-                        left += 1
-                        right -= 1
-                        while left < right and nums[left] == nums[left - 1]:
-                            left += 1
-                        while left < right and nums[right] == nums[right + 1]:
-                            right -= 1
-                    elif two_sum + nums[i] + nums[j] < target:
-                        left += 1
-                    else:
-                        right -= 1
-        return ans
-
-
-
-
-
-
-
-
-
-
         # O(n^4)
         # O(m)
         # n = len(nums)
@@ -67,46 +18,46 @@ class Solution:
         # return ans
         # O(n^3)
         # O(1)
-        # nums.sort()
-        # n = len(nums)
-        # ans = []
-        # for i in range(n - 3):
-        #     # Skip duplicate i
-        #     if i > 0 and nums[i - 1] == nums[i]:
-        #         continue
-        #     # Prune by min/max possible sums with fixed i
-        #     min_sum_i = nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3]
-        #     if min_sum_i > target:
-        #         break    # i increases -> min_sum_i only grows
-        #     max_sum_i = nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3]
-        #     if max_sum_i < target:
-        #         continue  # even the largest is too small
-        #     for j in range(i + 1, n - 2):
-        #         # Skip duplicate j under the same i
-        #         if j > i + 1 and nums[j - 1] == nums[j]:
-        #             continue
-        #         # Prune by min/max possible sums with fixed i, j
-        #         min_sum_ij = nums[i] + nums[j] + nums[j + 1] + nums[j + 2]
-        #         if min_sum_ij > target:
-        #             break
-        #         max_sum_ij = nums[i] + nums[j] + nums[n - 1] + nums[n - 2]
-        #         if max_sum_ij < target:
-        #             continue
+        nums.sort()
+        n = len(nums)
+        ans = []
+        for i in range(n - 3):
+            # Skip duplicate i
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
+            # Prune by min/max possible sums with fixed i
+            min_sum_i = nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3]
+            if min_sum_i > target:
+                break    # i increases -> min_sum_i only grows
+            max_sum_i = nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3]
+            if max_sum_i < target:
+                continue  # even the largest is too small
+            for j in range(i + 1, n - 2):
+                # Skip duplicate j under the same i
+                if j > i + 1 and nums[j - 1] == nums[j]:
+                    continue
+                # Prune by min/max possible sums with fixed i, j
+                min_sum_ij = nums[i] + nums[j] + nums[j + 1] + nums[j + 2]
+                if min_sum_ij > target:
+                    break
+                max_sum_ij = nums[i] + nums[j] + nums[n - 1] + nums[n - 2]
+                if max_sum_ij < target:
+                    continue
                 
-        #         left, right = j + 1, n - 1
-        #         need = target - nums[i] - nums[j]
-        #         while left < right:
-        #             two = nums[left] + nums[right]
-        #             if two == need:
-        #                 ans.append([nums[i], nums[j], nums[left], nums[right]])
-        #                 left += 1
-        #                 right -= 1
-        #                 while left < right and nums[left] == nums[left - 1]:
-        #                     left += 1
-        #                 while left < right and nums[right] == nums[right + 1]:
-        #                     right -= 1
-        #             elif two < need:
-        #                 left += 1
-        #             else:
-        #                 right -= 1
-        # return ans
+                left, right = j + 1, n - 1
+                need = target - nums[i] - nums[j]
+                while left < right:
+                    two = nums[left] + nums[right]
+                    if two == need:
+                        ans.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif two < need:
+                        left += 1
+                    else:
+                        right -= 1
+        return ans
