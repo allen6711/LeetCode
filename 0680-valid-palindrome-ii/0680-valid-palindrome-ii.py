f0fe08e2e77f09b35603d3c5361ece589919375b
allen6711 <allen2990167@gmail.com>
2026-02-20 23:47:27 -0800

    Time: 88 ms (8.53%), Space: 19.8 MB (16.21%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -2,55 +2,42 @@ class Solution:
     def validPalindrome(self, s: str) -> bool:
         left, right = 0, len(s) - 1
         while left < right:
-            if s[left] != s[right]:
-                return self.isPalindrome(s, left + 1, right) or self.isPalindrome(s, left, right - 1)
-            left += 1
-            right -= 1
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+            else:
+                return self.is_pal(s, left + 1, right) or self.is_pal(s, left, right - 1)
         
         return True
 
-    def isPalindrome(self, s, left, right) -> bool:
+    def is_pal(self, s: str, left: int, right: int) -> bool:
         while left < right:
-            if s[left] != s[right]:
+            if s[left].lower() != s[right].lower():
                 return False
             left += 1
             right -= 1
         
         return True
+            
 
-    #     left, right = self.twoPointer(s, 0, len(s) - 1)
-    #     if left >= right:
-    #         return True
-        
-    #     return self.is_palindrome(s, left + 1, right) or self.is_palindrome(s, left, right - 1)
 
-    # def is_palindrome(self, s, left, right):
-    #     left, right = self.twoPointer(s, left, right)
-    #     return left >= right
 
-    # def twoPointer(self, s, left, right):
+    #     left, right = 0, len(s) - 1
+
     #     while left < right:
     #         if s[left] != s[right]:
-    #             return left, right
-            
+    #             return self.isPalindrome(s, left + 1, right) or self.isPalindrome(s, left, right - 1)
     #         left += 1
     #         right -= 1
         
-    #     return left, right
+    #     return True
 
-        # def is_palindrome(left, right) -> bool:
-        #     while left < right:
-        #         if s[left] != s[right]:
-        #             return False
-        #         left += 1
-        #         right -= 1
-        #     return True
-        
-        # left, right = 0, len(s) - 1
-        # while left < right:
-        #     if s[left] != s[right]:
-        #         return is_palindrome(left, right - 1) or is_palindrome(left + 1, right)
-        #     left += 1
-        #     right -= 1
-        
-        # return True
+
+    # def isPalindrome(self, s, left, right):
+    #     while left < right:
+    #         if s[left] != s[right]:
+    #             return False
+    #         left += 1
+    #         right -= 1
+
+    #     return True 
