435deb735683c63225e34e97f36cc9f6e41af2d6
allen6711 <allen2990167@gmail.com>
2026-02-23 15:51:23 -0800

    Time: 0 ms (100%), Space: 19.8 MB (16.06%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -12,15 +12,3 @@ class Solution:
                 ans.append(str(i))
         
         return ans
-        # ans = []
-        # for i in range(n):
-        #     if (i + 1) % 3 == 0 and (i + 1) % 5 == 0:
-        #         ans.append("FizzBuzz")
-        #     elif (i + 1) % 3 == 0:
-        #         ans.append("Fizz")
-        #     elif (i + 1) % 5 == 0:
-        #         ans.append("Buzz")
-        #     else:
-        #         ans.append(str(i + 1))
-        
-        # return ans
