f9e54614e5751fae57cc095268bd21479754a76f
allen6711 <allen2990167@gmail.com>
2026-02-25 01:16:11 -0800

    Time: 3 ms (10.53%), Space: 19.5 MB (7.35%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -5,66 +5,49 @@
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        if not head:
-            return None
-        
-        small = []
-        large = []
-        cur = head
-        while cur:
-            if cur.val < x:
-                small.append(cur)
-            else:
-                large.append(cur)
-            cur = cur.next
-        
-        arr = small + large
-        if not arr:
-            return None
-        
-        for i in range(len(arr) - 1):
-            arr[i].next = arr[i + 1]
-        
-        arr[-1].next = None
-        
-        return arr[0]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         # O(n)
         # O(n)
-        if not head:
-            return None
-        small = []
-        large = []
+        # if not head:
+        #     return None
+        # small = []
+        # large = []
+        # cur = head
+        # while cur:
+        #     if cur.val < x:
+        #         small.append(cur)
+        #     else:
+        #         large.append(cur)
+        #     cur = cur.next
+        
+        # arr = small + large
+        
+        # for i in range(len(arr) - 1):
+        #     arr[i].next = arr[i + 1]
+        
+        # arr[-1].next = None
+
+        # return arr[0]
+        # O(n)
+        # O(1)
+        less_dummy = ListNode(0)
+        ge_dummy = ListNode(0)
+        less_tail = less_dummy
+        ge_tail = ge_dummy
+
         cur = head
         while cur:
+            # next_node = cur.next
             if cur.val < x:
-                small.append(cur)
+                less_tail.next = cur
+                less_tail = less_tail.next
             else:
-                large.append(cur)
-            cur = cur.next
-        
-        arr = small + large
-        if not arr:
-            return None
-        
-        for i in range(len(arr) - 1):
-            arr[i].next = arr[i + 1]
-        
-        arr[-1].next = None
+                ge_tail.next = cur
+                ge_tail = ge_tail.next
 
-        return arr[0]
+            cur = cur.next
+
+        ge_tail.next = None
+        less_tail.next = ge_dummy.next
+        return less_dummy.next
+
+        
