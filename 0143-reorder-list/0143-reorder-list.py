189acb238a5abd142c5306d391b10a35fab0b19e
allen6711 <allen2990167@gmail.com>
2026-02-26 20:52:18 -0800

    Time: 0 ms (100%), Space: 27.9 MB (37.9%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,6 +8,24 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        if not head or not head.next:
+            return None
+        
+        arr = []
+        cur = head
+        while cur:
+            arr.append(cur)
+            cur = cur.next
+        left, right = 0, len(arr) - 1
+        while left < right:
+            arr[left].next = arr[right]
+            left += 1
+            if left == right:
+                break
+            arr[right].next = arr[left]
+            right -= 1
+        
+        arr[left].next = None
         # O(n)
         # O(n)
         # if not head or not head.next:
@@ -30,37 +48,37 @@ class Solution:
         #     nodes[right].next = nodes[left]
         #     right -= 1
 
-        # nodes[left].next = None
-        # O(n)
-        # O(1)
-        if not head or not head.next:
-            return None
-        # 1. Find the end of the first half
-        slow, fast = head, head
-        while fast.next and fast.next.next:
-            slow = slow.next
-            fast = fast.next.next
+        # # nodes[left].next = None
+        # # O(n)
+        # # O(1)
+        # if not head or not head.next:
+        #     return None
+        # # 1. Find the end of the first half
+        # slow, fast = head, head
+        # while fast.next and fast.next.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
             
-        # 2. Revese the second half
-        second = slow.next
-        slow.next = None  # Split the list into two halves
-        prev = None
-        cur = second
-        while cur:
-            next_node = cur.next
-            cur.next = prev
-            prev = cur
-            cur = next_node
+        # # 2. Revese the second half
+        # second = slow.next
+        # slow.next = None  # Split the list into two halves
+        # prev = None
+        # cur = second
+        # while cur:
+        #     next_node = cur.next
+        #     cur.next = prev
+        #     prev = cur
+        #     cur = next_node
         
-        # 3. Merge the two halves alternately
-        first, second = head, prev
-        while second:
-            first_next = first.next
-            second_next = second.next
+        # # 3. Merge the two halves alternately
+        # first, second = head, prev
+        # while second:
+        #     first_next = first.next
+        #     second_next = second.next
 
-            first.next = second
-            second.next = first_next
+        #     first.next = second
+        #     second.next = first_next
 
-            first = first_next
-            second = second_next
+        #     first = first_next
+        #     second = second_next
         
