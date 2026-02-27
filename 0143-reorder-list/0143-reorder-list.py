5c63e0a797e2d111d136d8fdf9e30cd65dbe79bd
allen6711 <allen2990167@gmail.com>
2026-02-26 18:38:38 -0800

    Time: 0 ms (100%), Space: 27.8 MB (82.75%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,35 +8,6 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head or not head.next:
-            return None
-        
-        cur = head
-        nodes = []
-        while cur:
-            nodes.append(cur)
-            cur = cur.next
-        
-        left, right = 0, len(nodes) - 1
-        while left < right:
-            nodes[left].next = nodes[right]
-            left += 1
-            if left == right:
-                break
-            nodes[right].next = nodes[left]
-            right -= 1
-        
-        nodes[left].next = None
-
-
-
-
-
-
-
-
-
-
         # O(n)
         # O(n)
         # if not head or not head.next:
@@ -60,3 +31,36 @@ class Solution:
         #     right -= 1
 
         # nodes[left].next = None
+        # O(n)
+        # O(1)
+        if not head or not head.next:
+            return None
+        # 1. Find the end of the first half
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        # 2. Revese the second half
+        second = slow.next
+        slow.next = None  # Split the list into two halves
+        prev = None
+        cur = second
+        while cur:
+            next_node = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next_node
+        
+        # 3. Merge the two halves alternately
+        first, second = head, prev
+        while second:
+            first_next = first.next
+            second_next = second.next
+
+            first.next = second
+            second.next = first_next
+
+            first = first_next
+            second = second_next
+        
