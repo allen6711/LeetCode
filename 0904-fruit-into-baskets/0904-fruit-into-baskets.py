3d260c90f6551637d120c41e0770a1ee589414f6
allen6711 <allen2990167@gmail.com>
2026-02-28 17:09:57 -0800

    Time: 190 ms (54.97%), Space: 26.1 MB (29.78%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,46 +1,26 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        left = 0
-        count = {}
-        best = float('-inf')
-        for right, fruit in enumerate(fruits):
-            if fruit in count:
-                count[fruit] += 1
-            else:
-                count[fruit] = 1
-            
-            while len(count) > 2:
-                left_fruit = fruits[left]
-                count[left_fruit] -= 1
-
-                if count[left_fruit] == 0:
-                    del count[left_fruit]
+        # O(n^2)
+        # O(k)
+        # n = len(fruits)
+        # best = float('-inf')
+        # for i in range(n):
+        #     count = {}
+        #     for j in range(i, n):
+        #         if fruits[j] in count:
+        #             count[fruits[j]] += 1
+        #         else:
+        #             count[fruits[j]] = 1
                 
-                left += 1
-            
-            best = max(best, right - left + 1)
-        
-        return best
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        #         if len(count) > 2:
+        #             break
+        #         best = max(best, j - i + 1)
+        # return best
         # O(n)
         # O(k)
         left = 0
         count = {}
-        best = 0
+        best = float('-inf')
         for right, fruit in enumerate(fruits):
             if fruit in count:
                 count[fruit] += 1
