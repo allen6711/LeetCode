963ccbc8b6f359f252a858fad9814f65173674a2
allen6711 <allen2990167@gmail.com>
2026-03-02 17:09:49 -0800

    Time: 67 ms (63.34%), Space: 19.6 MB (86.52%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,27 +2,26 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if not s or not t:
             return ""
+        n = len(s)
+        need = Counter(t)
+        left = 0
         best_left = 0
         best_len = float('inf')
-        need = Counter(t)
         window = defaultdict(int)
-        now_have = 0
-        need_num_char = len(need)
-        left = 0
+        have = 0
         for right, char in enumerate(s):
             window[char] += 1
             if char in need and window[char] == need[char]:
-                now_have += 1
+                have += 1
             
-            while now_have == need_num_char:
+            while have == len(need):
                 if right - left + 1 < best_len:
                     best_len = right - left + 1
                     best_left = left
-
                 left_char = s[left]
                 window[left_char] -= 1
                 if left_char in need and window[left_char] < need[left_char]:
-                    now_have -= 1
+                    have -= 1
                 left += 1
         
         return "" if best_len == float('inf') else s[best_left:best_left + best_len]
