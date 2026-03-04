8c4c13342a0130e4bae8a5079d5b612a77c7ce41
allen6711 <allen2990167@gmail.com>
2026-03-04 07:57:14 -0800

    Time: 226 ms (95%), Space: 21.6 MB (88.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,5 +1,44 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        islands = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] != '1':
+                    continue
+                
+                islands += 1
+                grid[r][c] = '0'
+                q = deque([(r, c)])
+
+                while q:
+                    x, y = q.popleft()
+                    for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+                        nx, ny = x + dx, y + dy
+                        if 0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == '1':
+                            grid[nx][ny] = '0'
+                            q.append((nx, ny))
+        return islands
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # O(mn)
         # O(mn)
         if not grid and not grid[0]:
