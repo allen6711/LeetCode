b7968bbcf1d37ac562ab3ca21d5ba8848433c6fb
allen6711 <allen2990167@gmail.com>
2026-03-04 21:32:46 -0800

    Time: 173 ms (54.77%), Space: 21.1 MB (5.98%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,5 +1,37 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        if grid[0][0] == 1 or grid[n - 1][n - 1] == 1:
+            return -1
+        
+        q = deque([(0, 0, 1)])
+        visited = set([0, 0])
+        while q:
+            x, y, dist = q.popleft()
+            if x == n - 1 and y == n - 1:
+                return dist
+            for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)):
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == 0:
+                    if (nx, ny) not in visited:
+                        visited.add((nx, ny))
+                        q.append((nx, ny, dist + 1))
+        
+        return -1
+
+
+
+
+
+
+
+
+
+
+
+
+        # O(n^2)
+        # O(n^2)
         # If start of end is blocked, no path exists
         n = len(grid)
         if grid[0][0] == 1 or grid[n - 1][n - 1]:
