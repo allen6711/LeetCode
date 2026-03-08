1e3a3f37e184c8cd4c04a427857409e9370f6501
allen6711 <allen2990167@gmail.com>
2026-03-07 19:25:55 -0800

    Time: 7 ms (46.25%), Space: 20.3 MB (98.22%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,37 +1,5 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = [[] for _ in range(numCourses)]
-        indegree = [0] * numCourses
-
-        for course, pre in prerequisites:
-            graph[pre].append(course)
-            indegree[course] += 1
-        
-        q = deque()
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                q.append(i)
-        taken = 0
-        while q:
-            cur = q.popleft()
-            taken += 1
-            for next_course in graph[cur]:
-                indegree[next_course] -= 1
-                if indegree[next_course] == 0:
-                    q.append(next_course)
-        return taken == numCourses
-
-
-
-
-
-
-
-
-
-
-
-
         # O(V+E), V: numCourses, E: len(prerequisities)
         # O(V+E)
         # Build graph: pre -> list of courses that depend on it
