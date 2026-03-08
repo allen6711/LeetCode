279a693faef63b3ae5f188a79962f15cdd6ae094
allen6711 <allen2990167@gmail.com>
2026-03-07 22:43:29 -0800

    Time: 0 ms (100%), Space: 23.1 MB (42.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,5 +1,38 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # O(n+mlogk)
+        # O(m)
+        counts = Counter(nums)
+        
+        heap = []  # (freq, num)
+        for num, freq in counts.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (freq, num))
+            
+            else:
+                # Keep the k highest frequencies in the heap
+                if freq > heap[0][0]:
+                    heapq.heapreplace(heap, (freq, num))
+        
+        return [num for freq, num in heap]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         n = len(nums)
         answer = []
         freq = Counter(nums)
@@ -18,20 +51,6 @@ class Solution:
                     return answer
         
         return answer
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
         # n = len(nums)
