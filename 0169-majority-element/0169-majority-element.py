8d3bcbb3ccbf826fe9fa4db4b315552b3f04922e
allen6711 <allen2990167@gmail.com>
2026-03-15 16:20:00 -0700

    Time: 3 ms (86.07%), Space: 21.1 MB (96.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,27 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = 0
-        candidate = None
-        for num in nums:
-            if count == 0:
-                candidate = num
-                count = 1
-            elif num == candidate:
-                count += 1
-            else:
-                count -= 1
-        return candidate
-        
-
-
-
-
-
-
-
-
-
-
         # O(n)
         # O(n)
         # freq = defaultdict(int)
