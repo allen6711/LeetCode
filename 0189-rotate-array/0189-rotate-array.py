35dc039bc4f451a64a9a4286c21e65c062a7d45f
allen6711 <allen2990167@gmail.com>
2026-03-19 22:02:28 -0700

    Time: 9 ms (27.01%), Space: 26.5 MB (84.66%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,16 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # O(n)
-        # O(n)
         n = len(nums)
         k %= n
         res = [0] * n
-
         for i in range(n):
             res[(i + k) % n] = nums[i]
-        
         for i in range(n):
             nums[i] = res[i]
+        # O(n)
+        # O(n)
+        # n = len(nums)
+        # k %= n
+        # res = [0] * n
+
+        # for i in range(n):
+        #     # move k step to right
+        #     res[(i + k) % n] = nums[i]
+        
+        # for i in range(n):
+        #     nums[i] = res[i]
 
          
