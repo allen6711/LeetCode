5e26bd82319d0ca28bcbb1b11b8cbc92903aad34
allen6711 <allen2990167@gmail.com>
2026-03-19 22:10:23 -0700

    Time: 7 ms (56.39%), Space: 27.2 MB (6.73%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,13 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        k %= n
-        res = [0] * n
-        for i in range(n):
-            res[(i + k) % n] = nums[i]
-        for i in range(n):
-            nums[i] = res[i]
         # O(n)
         # O(n)
         # n = len(nums)
@@ -22,5 +15,20 @@ class Solution:
         
         # for i in range(n):
         #     nums[i] = res[i]
+        
+        # O(n)
+        # O(1)
+        n = len(nums)
+        k %= n
+        self.reverse(0, n - 1, nums)
+        self.reverse(0, k - 1, nums)
+        self.reverse(k, n - 1, nums)
+
+    def reverse(self, left: int, right: int, nums: list[int]) -> None:
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+        
 
          
