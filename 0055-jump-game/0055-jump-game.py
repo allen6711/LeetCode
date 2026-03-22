46beaf85224e289d37a812b1181989789a782771
allen6711 <allen2990167@gmail.com>
2026-03-21 23:11:45 -0700

    Time: 10 ms (93.32%), Space: 20.2 MB (93.55%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,5 +1,28 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        farthest = 0
+        n = len(nums)
+        for i in range(n):
+            if i > farthest:
+                return False
+            farthest = max(farthest, nums[i] + i)
+            if farthest >= n - 1:
+                return True
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # O(n)
         # O(1)
         farthest = 0
