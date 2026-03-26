318377e8f6635f7e8036410cda601586ba746598
allen6711 <allen2990167@gmail.com>
2026-03-25 18:16:05 -0700

    Time: 347 ms (5%), Space: 19.5 MB (50.39%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,5 +1,29 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        best = 0
+        for i in range(1, n + 1):
+            check = 0
+            for num in citations:
+                if num >= i:
+                    check += 1
+            if check >= i:
+                best = max(best, i)
+        
+        return best
+
+
+
+
+
+
+
+
+
+
+
+
+
         # O(n^2)
         # O(1)
         # n = len(citations)
