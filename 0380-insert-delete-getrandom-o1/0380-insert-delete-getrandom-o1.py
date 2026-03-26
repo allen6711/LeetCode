1b470b8979b6d583476c55b0a1b42b6e8bbfcaf1
allen6711 <allen2990167@gmail.com>
2026-03-26 15:41:37 -0700

    Time: 66 ms (98.75%), Space: 57.2 MB (18.47%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -2,29 +2,24 @@ class RandomizedSet:
     def __init__(self):
         self.nums = []
         self.pos = {}
-    
     def insert(self, val: int) -> bool:
         if val in self.pos:
             return False
-        # insert to the last + 1 position
         self.pos[val] = len(self.nums)
         self.nums.append(val)
         return True
-    
     def remove(self, val: int) -> bool:
-        if val not in self.pos[val]:
+        if val not in self.pos:
             return False
-        last_val = self.nums[-1]
         remove_idx = self.pos[val]
-        self.nums[remove_idx] = last_val
+        last_val = self.nums[-1]
         self.pos[last_val] = remove_idx
+        self.nums[remove_idx] = last_val
         self.nums.pop()
         del self.pos[val]
         return True
-    
     def getRandom(self) -> int:
         return random.choice(self.nums)
-
 
 
 
