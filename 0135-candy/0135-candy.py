f5ffb2abee04bbd7bd3fbf9a8d775b6d6e896038
allen6711 <allen2990167@gmail.com>
2026-03-27 22:36:11 -0700

    Time: 14 ms (74.09%), Space: 21.3 MB (87.91%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,5 +1,15 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        candies = [1] * n
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+        return sum(candies)
         # O(n^2)
         # O(n)
         # n = len(ratings)
@@ -17,7 +27,7 @@ class Solution:
         #             changed = True
         # return sum(candies)
         # O(n)
-        # O()
+        # O(n)
         n = len(ratings)
         candies = [1] * n
         # Left to right: satisfy left neighbor rule
