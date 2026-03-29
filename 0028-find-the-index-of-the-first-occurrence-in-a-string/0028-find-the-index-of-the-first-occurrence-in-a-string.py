b7e875804701bb0ba7f737633d7a4279163628ac
allen6711 <allen2990167@gmail.com>
2026-03-28 22:33:43 -0700

    Time: 1 ms (25.92%), Space: 19.4 MB (19.98%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -14,38 +14,39 @@ class Solution:
         return -1
         # O(n + m)
         # O(m)
-        if needle == "":
-            return 0
-        n = len(haystack)
-        m = len(needle)
+        # if needle == "":
+        #     return 0
+        # n = len(haystack)
+        # m = len(needle)
 
-        lps = [0] * m
-        length = 0
-        i = 1
+        # # longest prefix suffix
+        # lps = [0] * m
+        # length = 0
+        # i = 1
 
-        while i < m:
-            if needle[i] == needle[length]:
-                length += 1
-                lps[i] = length
-                i += 1
-            else:
-                if length != 0:
-                    length = lps[length - 1]
-                else:
-                    lps[i] = 0
-                    i += 1
-        i = 0  # pointer for haystack
-        j = 0  # pointer for needle
-        while i < n:
-            if haystack[i] == needle[j]:
-                i += 1
-                j += 1
+        # while i < m:
+        #     if needle[i] == needle[length]:
+        #         length += 1
+        #         lps[i] = length
+        #         i += 1
+        #     else:
+        #         if length != 0:
+        #             length = lps[length - 1]
+        #         else:
+        #             lps[i] = 0
+        #             i += 1
+        # i = 0  # pointer for haystack
+        # j = 0  # pointer for needle
+        # while i < n:
+        #     if haystack[i] == needle[j]:
+        #         i += 1
+        #         j += 1
 
-                if j == m:
-                    return i - m
-            else:
-                if j != 0:
-                    j = lps[j - 1]
-                else:
-                    i += 1
-        return -1
+        #         if j == m:
+        #             return i - m
+        #     else:
+        #         if j != 0:
+        #             j = lps[j - 1]
+        #         else:
+        #             i += 1
+        # return -1
