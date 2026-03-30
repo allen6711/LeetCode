d84ab01111e768519583254714d27b4a2089dd58
allen6711 <allen2990167@gmail.com>
2026-03-29 18:23:49 -0700

    Time: 23 ms (52.06%), Space: 19.6 MB (85.16%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,6 +1,16 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        count = {}
+        for ch in magazine:
+            count[ch] = count.get(ch, 0) + 1
         
+        for ch in ransomNote:
+            if count.get(ch, 0) == 0:
+                return False
+            else:
+                count[ch] -= 1
+        
+        return True
         # O(n*m)
         # O(m)
         # magazine = list(magazine)
