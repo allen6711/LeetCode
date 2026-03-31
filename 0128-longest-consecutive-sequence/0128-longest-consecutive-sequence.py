17f61e21cf7b0bbb35a6e70166c7861919662bbe
allen6711 <allen2990167@gmail.com>
2026-03-31 09:52:44 -0700

    Time: 37 ms (96.46%), Space: 36.7 MB (30.64%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,58 +1,37 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-
-
-
-
-
-
-
-
-
         # O(nlogn)
         # O(1)
-        if not nums:
-            return 0
-        nums.sort()
-        longest = 1
-        current = 1
-        n = len(nums)
-        for i in range(1, n):
-            if nums[i] == nums[i - 1]:
-                continue
-            elif nums[i] == nums[i - 1] + 1:
-                current += 1
-            else:
-                current = 1
+        # if not nums:
+        #     return 0
+        # nums.sort()
+        # longest = 1
+        # current = 1
+        # n = len(nums)
+        # for i in range(1, n):
+        #     if nums[i] == nums[i - 1]:
+        #         continue
+        #     elif nums[i] == nums[i - 1] + 1:
+        #         current += 1
+        #     else:
+        #         current = 1
         
-            longest = max(longest, current)
+        #     longest = max(longest, current)
 
+        # return longest
+        # O(n)
+        # O(n)
+        num_set = set(nums)
+        longest = 0
+        current = 0
+        for num in num_set:
+            if num - 1 not in num_set:  # find the sequence start
+                current = num
+                length = 1
+
+                while current + 1 in num_set:
+                    current += 1
+                    length += 1
+                
+                longest = max(longest, length)
         return longest
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # O(n)
-        # O(n)
-        s = set(nums)
-        best = 0
-        for num in s:
-            if num - 1 in s:
-                continue    # not a sequence start
-            num2 = num
-            while num2 in s:
-                num2 += 1
-            
-            best = max(best, num2 - num)  # length
-            
-        return best
