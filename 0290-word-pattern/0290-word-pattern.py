df6359863ca8536ca4f3a1b283951008294ef336
allen6711 <allen2990167@gmail.com>
2026-03-30 22:03:30 -0700

    Time: 0 ms (100%), Space: 19.1 MB (96.77%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        p_to_s = {}
+        s_to_p = {}
+        for char, word in zip(pattern, words):
+            if char in p_to_s and p_to_s[char] != word:
+                return False
+            if word in s_to_p and s_to_p[word] != char:
+                return False
+            p_to_s[char] = word
+            s_to_p[word] = char
+
+        return True
