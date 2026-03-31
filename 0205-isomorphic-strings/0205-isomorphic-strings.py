9ed299df0263bba16cb2a115d576ec8330d457a6
allen6711 <allen2990167@gmail.com>
2026-03-30 21:41:55 -0700

    Time: 3 ms (91.33%), Space: 19.2 MB (65.07%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,5 +1,15 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        s_to_t = {}
+        t_to_s = {}
+        for ch_s, ch_t in zip(s, t):
+            if ch_s in s_to_t and s_to_t[ch_s] != ch_t:
+                return False
+            if ch_t in t_to_s and t_to_s[ch_t] != ch_s:
+                return False
+            s_to_t[ch_s] = ch_t
+            t_to_s[ch_t] = ch_s
+        return True
         # O(n^2)
         # O(1)
         # n = len(s)
@@ -9,8 +19,8 @@ class Solution:
         #             return False
         
         # return True
-        # O(n+m)
-        # O(1)
+        # O(n)
+        # O(n)
         s_to_t = {}
         t_to_s = {}
         for char_s, char_t in zip(s, t):
