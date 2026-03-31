73b166ec0e9025d17c33c4cb618a4493bf47b5d7
allen6711 <allen2990167@gmail.com>
2026-03-30 22:23:02 -0700

    Time: 12 ms (54.4%), Space: 19.3 MB (85.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,7 +5,20 @@ class Solution:
         # return sorted(s) == sorted(t)
         # O(n)
         # O(k)
-        return Counter(s) == Counter(t)
+        # return Counter(s) == Counter(t)
+        # O(n)
+        # O(1)
+        if len(s) != len(t):
+            return False
+        freq = [0] * 26
+        for ch in s:
+            freq[ord(ch) - ord('a')] += 1
+        for ch in t:
+            freq[ord(ch) - ord('a')] -= 1
+        for count in freq:
+            if count != 0:
+                return False
+        return True
 
 
 
