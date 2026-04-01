da1c583e1a9ca07937cb5e333a78faf716f38f69
allen6711 <allen2990167@gmail.com>
2026-04-01 00:28:41 -0700

    Time: 10 ms (77.96%), Space: 19.4 MB (85.1%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,30 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        freq = [0] * 26
+        for ch in s:
+            freq[ord(ch) - ord('a')] += 1
+        for ch in t:
+            freq[ord(ch) - ord('a')] -= 1
+        
+        for count in freq:
+            if count != 0:
+                return False
+        
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # O(nlogn)
         # O(n)
         # return sorted(s) == sorted(t)
@@ -19,103 +44,3 @@ class Solution:
             if count != 0:
                 return False
         return True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        if len(s) != len(t):
-            return False
-        
-        cnt = [0] * 26
-        base = ord('a')
-
-        for char in s:
-            cnt[ord(char) - base] += 1
-        
-        for char in t:
-            cnt[ord(char) - base] -= 1
-            if cnt[ord(char) - base] < 0:
-                return False
-        
-        return True
-
-        # cnt = [0] * 26
-        # base = ord('a')
-
-        # for char in s:
-        #     cnt[ord(char) - base] += 1
-        
-        # for char in t:
-        #     cnt[ord(char) - base] -= 1
-        
-        # return all(x == 0 for x in cnt)
-
-
-        # cnt = [0] * 26
-        # base = ord('a')
-
-        # for char in s:
-        #     cnt[ord(char) - base] += 1
-        
-        # for char in t:
-        #     cnt[ord(char) - base] -= 1
-        
-        # return all(x == 0 for x in cnt)
-
-        # if len(s) != len(t):
-        #     return False
-            
-        # cnt = [0] * 26
-        # base = ord('a')
-
-        # for char in s:
-        #     cnt[ord(char) - base] += 1
-        
-        # for char in t:
-        #     cnt[ord(char) - base] -= 1
-        
-        # return all(x == 0 for x in cnt)
-
-
-
-        # if len(s) != len(t):
-        #     return False
-            
-        # cnt = [0] * 26
-        # base = ord('a')
-
-        # for ch in s:
-        #     cnt[ord(ch) - base] += 1
-        
-        # for ch in t:
-        #     cnt[ord(ch) - base] -= 1
-        
-        # return all(x == 0 for x in cnt)
-
-        # if len(s) != len(t):
-        #     return False
-
-        # cnt = [0] * 26
-        # base = ord("a")
-
-        # for ch in s:
-        #     cnt[ord(ch) - base] += 1
-        
-        # for ch in t:
-        #     index = ord(ch) - base
-        #     cnt[index] -= 1
-            
-        #     if cnt[index] < 0:
-        #         return False
-
-        # return True
