ddc53e842a10bc9e55ed80a7ff087df4cb9b8017
allen6711 <allen2990167@gmail.com>
2026-04-01 00:31:51 -0700

    Time: 11 ms (75.74%), Space: 19.4 MB (60.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,29 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        freq = [0] * 26
-        for ch in s:
-            freq[ord(ch) - ord('a')] += 1
-        for ch in t:
-            freq[ord(ch) - ord('a')] -= 1
-        
-        for count in freq:
-            if count != 0:
-                return False
-        return True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         # O(nlogn)
         # O(n)
         # return sorted(s) == sorted(t)
