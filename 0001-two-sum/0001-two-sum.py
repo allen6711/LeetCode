031bc92ea2e4a3c6f2fec89e0c6d38261942864e
allen6711 <allen2990167@gmail.com>
2026-04-03 15:19:13 -0700

    Time: 2 ms (55.66%), Space: 20.6 MB (30.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,5 +1,27 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        collect = {}
+        for i, num in enumerate(nums):
+            new_target = target - num
+            if new_target in collect:
+                return [collect[new_target], i]
+            collect[num] = i
+        
+        return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # O(n^2)
         # O(1)
         # n = len(nums)
