2f2ec24abbefc4e553985470e286275a1a915e66
allen6711 <allen2990167@gmail.com>
2026-04-02 19:22:11 -0700

    Time: 16 ms (30.5%), Space: 24.1 MB (13.52%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,5 +1,23 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for word in strs:
+            freq = [0] * 26
+            for ch in word:
+                freq[ord(ch) - ord('a')] += 1
+            key = tuple(freq)
+            groups[key].append(word)
+        
+        return list(groups.values())
+
+
+
+
+
+
+
+
+
         # O(klogk)
         # O(n*klogk)
         # groups = defaultdict(list)
