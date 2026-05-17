9ce934bb7b2217dc93fb1e04d43dc78812f7dbb8
allen6711 <allen2990167@gmail.com>
2026-05-17 16:50:11 -0700

    Time: 57 ms (12.17%), Space: 19.8 MB (55.25%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -11,10 +11,9 @@ class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return None
-        
+
         old_to_new = {}
         old_to_new[node] = Node(node.val)
-
         q = deque([node])
 
         while q:
@@ -24,7 +23,6 @@ class Solution:
                     old_to_new[nei] = Node(nei.val)
                     q.append(nei)
                 
-                # Add new nei -> old_to_new[nei] not origin nei
                 old_to_new[cur].neighbors.append(old_to_new[nei])
         
         return old_to_new[node]
