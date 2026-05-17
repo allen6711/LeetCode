36aaffb9471291757e87d13927b7d29396881ab5
allen6711 <allen2990167@gmail.com>
2026-05-17 16:50:26 -0700

    Time: 51 ms (49.37%), Space: 19.7 MB (86.83%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -9,38 +9,6 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node:
-            return None
-
-        old_to_new = {}
-        old_to_new[node] = Node(node.val)
-        q = deque([node])
-
-        while q:
-            cur = q.popleft()
-            for nei in cur.neighbors:
-                if nei not in old_to_new:
-                    old_to_new[nei] = Node(nei.val)
-                    q.append(nei)
-                
-                old_to_new[cur].neighbors.append(old_to_new[nei])
-        
-        return old_to_new[node]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         # BFS
         # O(V + E)
         # O(V)
