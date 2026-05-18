eb8b4259e58f74fe8d58cfa0beb860080fcae8a6
allen6711 <allen2990167@gmail.com>
2026-05-17 22:13:33 -0700

    Time: 0 ms (100%), Space: 19.5 MB (39.57%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,43 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = defaultdict(list)
+        # Build graph
+        for (a, b), value in zip(equations, values):
+            graph[a].append((b, value))
+            graph[b].append((a, 1 / value))
+        
+        def bfs(start: str, target: str) -> float:
+            # If either variable does not exist, answer is -1.0
+            if start not in graph or target not in graph:
+                return  -1.0
+            
+            # x / x = 1.0 if x exists in graph
+            if start == target:
+                return 1.0
+            
+            q = deque([(start, 1.0)])
+            visited = set([start])
+
+            while q:
+                cur, cur_value = q.popleft()
+
+                for nei, weight in graph[cur]:
+                    if nei in visited:
+                        continue
+                    
+                    next_value = cur_value * weight
+
+                    if nei == target:
+                        return next_value
+                    
+                    visited.add(nei)
+                    q.append((nei, next_value))
+            
+            return -1.0
+        
+        ans = []
+
+        for a, b in queries:
+            ans.append(bfs(a, b))
+        
+        return ans
