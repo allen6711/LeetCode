d70cbe8ce6a7e80ac4f6f03ca4025ae4d2437d37
allen6711 <allen2990167@gmail.com>
2026-05-20 08:49:13 -0700

    Time: 3 ms (75.09%), Space: 20.3 MB (73.96%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,5 +1,62 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        indegree = [0] * numCourses
+
+        for course, pre in prerequisites:
+            graph[pre].append(course)
+            indegree[course] += 1
+        
+        q = deque()
+        ans = []
+        completed = 0
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                q.append(course)
+            
+        while q:
+            cur = q.popleft()
+            ans.append(cur)
+            completed += 1
+            for next_course in graph[cur]:
+                indegree[next_course] -= 1
+
+                if indegree[next_course] == 0:
+                    q.append(next_course)
+        
+        return ans if completed == numCourses else []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # BFS
         n = numCourses
         node_indegree = {x: 0 for x in range(n)}
