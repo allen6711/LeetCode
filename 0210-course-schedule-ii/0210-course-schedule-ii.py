f586a8dfe23a628f9b74c6ce483e6de2d6360754
allen6711 <allen2990167@gmail.com>
2026-05-21 16:11:42 -0700

    Time: 7 ms (28.17%), Space: 20.1 MB (99.66%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,44 +1,5 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
-        indegree = [0] * numCourses
-        for course, pre in prerequisites:
-            graph[pre].append(course)
-            indegree[course] += 1
-        
-        completed = 0
-        q = deque()
-        for course in range(numCourses):
-            if indegree[course] == 0:
-                q.append(course)
-        
-        ans = []
-        while q:
-            cur = q.popleft()
-            completed += 1
-            ans.append(cur)
-            for next_course in graph[cur]:
-                indegree[next_course] -= 1
-                
-                if indegree[next_course] == 0:
-                    q.append(next_course)
-        
-        return ans if completed == numCourses else []
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         # V = numCourses
         # E = len(prerequisites)
         # O(V+E)
