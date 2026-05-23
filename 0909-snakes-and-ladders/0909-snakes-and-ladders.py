1c630ae72175668834d121107d416923f8ee5222
allen6711 <allen2990167@gmail.com>
2026-05-22 21:44:32 -0700

    Time: 27 ms (34.18%), Space: 19.5 MB (19.14%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,58 +1,5 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        n = len(board)
-        target = n * n
-        def get_pos(square: int) -> tuple[int, int]:
-            idx = square - 1
-            row_from_bottom = idx // n
-            row = n - row_from_bottom - 1
-            col = idx % n
-            if row_from_bottom % 2 == 1:
-                col = n - col - 1
-            
-            return row, col
-        
-        q = deque([(1, 0)])
-        visited = set([1])
-        while q:
-            cur, moves = q.popleft()
-            if cur == target:
-                return moves
-            
-            for dice in range(1, 7):
-                next_square = cur + dice
-
-                if next_square > target:
-                    break
-                
-                r, c = get_pos(next_square)
-
-                if board[r][c] != -1:
-                    next_square = board[r][c]
-                
-                if next_square in visited:
-                    continue
-
-                visited.add(next_square)
-                q.append((next_square, moves + 1))
-        
-        return -1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         # O(n^2)
         # O(n^2)
         n = len(board)
