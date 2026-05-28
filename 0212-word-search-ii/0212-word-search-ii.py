1d0ea0c73f5dae7280fd10f8f0788019207f02f6
allen6711 <allen2990167@gmail.com>
2026-05-28 00:30:26 -0700

    Time: 4737 ms (56.92%), Space: 21.1 MB (58.23%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -3,20 +3,20 @@ class TrieNode:
         self.children = {}
         self.word = None
 
-class Sulution:
+class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         root = TrieNode()
 
         for word_in_list in words:
             cur = root
-            for ch in word:
+            for ch in word_in_list:
                 if ch not in cur.children:
-                    cur.children[ch] = TrieNode
+                    cur.children[ch] = TrieNode()
                 cur = cur.children[ch]
             cur.word = word_in_list
         
         rows, cols = len(board), len(board[0])
-        result = []
+        results = []
 
         def dfs(x: int, y: int, node: TrieNode) -> None:
             ch = board[x][y]
@@ -25,7 +25,8 @@ class Sulution:
             
             next_node = node.children[ch]
             if next_node.word is not None:
-                result.append(next_node.word)
+                results.append(next_node.word)
+                next_node.word = None
             
             board[x][y] = "#"
 
@@ -34,6 +35,11 @@ class Sulution:
                 if 0 <= nx < rows and 0 <= ny < cols and board[nx][ny] != "#":
                     dfs(nx, ny, next_node)
             
+            board[x][y] = ch
+        
+        for r in range(rows):
+            for c in range(cols):
+                dfs(r, c, root)
         return results
             
 
@@ -48,61 +54,61 @@ class Sulution:
 # O(M * N * 4 * 3^(K - 1))
 # O(min(K, M * N))
 
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.word = None
+# class TrieNode:
+#     def __init__(self):
+#         self.children = {}
+#         self.word = None
 
-class Solution:
-    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        root = TrieNode()
+# class Solution:
+#     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+#         root = TrieNode()
 
-        # Build Trie
-        for word_in_list in words:
-            cur = root
-            for ch in word_in_list:
-                if ch not in cur.children:
-                    cur.children[ch] = TrieNode()
-                cur = cur.children[ch]
-            cur.word = word_in_list
-        # root
-        #     └── o
-        #         └── a
-        #             └── t
-        #                 └── h  word = "oath"
+#         # Build Trie
+#         for word_in_list in words:
+#             cur = root
+#             for ch in word_in_list:
+#                 if ch not in cur.children:
+#                     cur.children[ch] = TrieNode()
+#                 cur = cur.children[ch]
+#             cur.word = word_in_list
+#         # root
+#         #     └── o
+#         #         └── a
+#         #             └── t
+#         #                 └── h  word = "oath"
         
-        rows = len(board)
-        cols = len(board[0])
-        result = []
+#         rows = len(board)
+#         cols = len(board[0])
+#         result = []
 
-        def dfs(r: int, c: int, node: TrieNode) -> None:
-            ch = board[r][c]
+#         def dfs(r: int, c: int, node: TrieNode) -> None:
+#             ch = board[r][c]
 
-            # If current character is not a valid next Trie path, stop.
-            if ch not in node.children:
-                return
+#             # If current character is not a valid next Trie path, stop.
+#             if ch not in node.children:
+#                 return
             
-            next_node = node.children[ch]
+#             next_node = node.children[ch]
 
-            # If this Trie node stores a word, we found one.
-            if next_node.word is not None:
-                result.append(next_node.word)
-                next_node.word = None      # Avoid duplicate results
+#             # If this Trie node stores a word, we found one.
+#             if next_node.word is not None:
+#                 result.append(next_node.word)
+#                 next_node.word = None      # Avoid duplicate results
             
-            # Mark current cell as visited
-            board[r][c] = "#"
+#             # Mark current cell as visited
+#             board[r][c] = "#"
 
-            for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-                nx, ny = r + dx, c + dy
-                if 0 <= nx < rows and 0 <= ny < cols and board[nx][ny] != "#":
-                    dfs(nx, ny, next_node)
+#             for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+#                 nx, ny = r + dx, c + dy
+#                 if 0 <= nx < rows and 0 <= ny < cols and board[nx][ny] != "#":
+#                     dfs(nx, ny, next_node)
 
-            # Restore current cell
-            board[r][c] = ch
+#             # Restore current cell
+#             board[r][c] = ch
 
-        for r in range(rows):
-            for c in range(cols):
-                dfs(r, c, root)
+#         for r in range(rows):
+#             for c in range(cols):
+#                 dfs(r, c, root)
 
-        return result 
+#         return result 
         
