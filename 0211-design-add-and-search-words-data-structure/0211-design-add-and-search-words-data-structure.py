6201e4edda949ac6df002e7bb9d372415420a31b
allen6711 <allen2990167@gmail.com>
2026-05-28 15:46:35 -0700

    Time: 936 ms (62.38%), Space: 69.4 MB (15.76%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -4,101 +4,50 @@ class TrieNode:
         self.is_word = False
 
 class WordDictionary:
+
     def __init__(self):
         self.root = TrieNode()
-    
+
+    # l: length of word
+    # O(l)
+    # O(l)
     def addWord(self, word: str) -> None:
         cur = self.root
+
         for ch in word:
             if ch not in cur.children:
                 cur.children[ch] = TrieNode()
-            cur = cur.children[ch]
 
+            cur = cur.children[ch]
+        
         cur.is_word = True
 
+    # O(l) without ".", O(26^l) with "."
+    # O(l)
     def search(self, word: str) -> bool:
-        def dfs(index: int, node: TrieNode) -> None:
+
+        def dfs(index: int, node: TrieNode) -> bool:
+            # If we have checked all characters
+            # the current node must be the end of a complete word.
             if index == len(word):
                 return node.is_word
-
+            
             ch = word[index]
 
+            # Case 1: normal charachter
             if ch != ".":
                 if ch not in node.children:
                     return False
                 return dfs(index + 1, node.children[ch])
             
+            # Case 2: wildcard "."
             for child in node.children.values():
                 if dfs(index + 1, child):
                     return True
             
             return False
-    
+        
         return dfs(0, self.root)
-                
-
-            
-
-
-
-
-
-
-
-
-
-
-
-# class TrieNode:
-#     def __init__(self):
-#         self.children = {}
-#         self.is_word = False
-
-# class WordDictionary:
-
-#     def __init__(self):
-#         self.root = TrieNode()
-
-#     # l: length of word
-#     # O(l)
-#     # O(l)
-#     def addWord(self, word: str) -> None:
-#         cur = self.root
-
-#         for ch in word:
-#             if ch not in cur.children:
-#                 cur.children[ch] = TrieNode()
-
-#             cur = cur.children[ch]
-        
-#         cur.is_word = True
-
-#     # O(l) without ".", O(26^l) with "."
-#     # O(l)
-#     def search(self, word: str) -> bool:
-
-#         def dfs(index: int, node: TrieNode) -> bool:
-#             # If we have checked all characters
-#             # the current node must be the end of a complete word.
-#             if index == len(word):
-#                 return node.is_word
-            
-#             ch = word[index]
-
-#             # Case 1: normal charachter
-#             if ch != ".":
-#                 if ch not in node.children:
-#                     return False
-#                 return dfs(index + 1, node.children[ch])
-            
-#             # Case 2: wildcard "."
-#             for child in node.children.values():
-#                 if dfs(index + 1, child):
-#                     return True
-            
-#             return False
-        
-#         return dfs(0, self.root)
         
 
 
