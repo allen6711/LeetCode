7ceca96d87376cf832158b314f425174e01107db
allen6711 <allen2990167@gmail.com>
2026-06-03 16:55:41 -0700

    Time: 191 ms (7.89%), Space: 61.5 MB (10.89%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -5,6 +5,20 @@ class Solution:
         def dfs(start: int) -> None:
             if len(path) == k:
                 result.append(path[:])
+            for num in range(start, n + 1):
+                path.append(num)
+                dfs(num + 1)
+                path.pop()
+        
+        dfs(1)
+        return result
+        # O(k*C(n, k))
+        # O(k*C(n, k))
+        result = []
+        path = []
+        def dfs(start: int) -> None:
+            if len(path) == k:
+                result.append(path[:])
                 return
             
             for num in range(start, n + 1):
