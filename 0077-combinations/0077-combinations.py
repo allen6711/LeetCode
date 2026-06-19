274265fc61f21c8e0ca3f7f32659610c9a5f3862
allen6711 <allen2990167@gmail.com>
2026-06-19 07:57:07 -0700

    Time: 113 ms (37.81%), Space: 61.3 MB (84.02%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,17 +1,5 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        result = []
-        path = []
-        def dfs(start: int) -> None:
-            if len(path) == k:
-                result.append(path[:])
-            for num in range(start, n + 1):
-                path.append(num)
-                dfs(num + 1)
-                path.pop()
-        
-        dfs(1)
-        return result
         # O(k*C(n, k))
         # O(k*C(n, k))
         result = []
