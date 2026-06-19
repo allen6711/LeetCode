729ef3f25b6b389c03481be92103ea2e040c1088
allen6711 <allen2990167@gmail.com>
2026-06-19 06:51:51 -0700

    Time: 0 ms (100%), Space: 19.3 MB (77.51%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,6 +2,7 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
+
         phone = {
             "2": "abc",
             "3": "def",
@@ -14,11 +15,10 @@ class Solution:
         }
 
         path = []
-        result = []
-
+        results = []
         def dfs(index: int) -> None:
             if index == len(digits):
-                result.append("".join(path))
+                results.append("".join(path))
                 return
             
             digit = digits[index]
@@ -28,6 +28,49 @@ class Solution:
                 path.pop()
         
         dfs(0)
-        return result
+        return results
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # if not digits:
+        #     return []
+        # phone = {
+        #     "2": "abc",
+        #     "3": "def",
+        #     "4": "ghi",
+        #     "5": "jkl",
+        #     "6": "mno",
+        #     "7": "pqrs",
+        #     "8": "tuv",
+        #     "9": "wxyz"
+        # }
+
+        # path = []
+        # result = []
+
+        # def dfs(index: int) -> None:
+        #     if index == len(digits):
+        #         result.append("".join(path))
+        #         return
+            
+        #     digit = digits[index]
+        #     for ch in phone[digit]:
+        #         path.append(ch)
+        #         dfs(index + 1)
+        #         path.pop()
+        
+        # dfs(0)
+        # return result
 
                 
