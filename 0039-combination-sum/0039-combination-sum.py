fa481b97c04520ea386a621940e614347c93beaf
allen6711 <allen2990167@gmail.com>
2026-06-21 00:39:36 -0700

    Time: 9 ms (54.01%), Space: 19.7 MB (8.96%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,42 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        results = []
+        path = []
+        def dfs(start, remain):
+            if remain == 0:
+                results.append(path[:])
+                return
+            if remain < 0:
+                return
+            
+            for i in range(start, len(candidates)):
+                path.append(candidates[i])
+                dfs(i, remain - candidates[i])
+                path.pop()
+        
+        dfs(0, target)
+        return results
+        # n = len(candidates)
+        # T = target
+        # m = min(candidates)
+        # O(n^(T/m))
+        # O(k × T/m)
+
+        results = []
+        path = []
+
+        def dfs(start: int, remain: int) -> None:
+            if remain == 0:
+                results.append(path[:])
+                return
+            
+            if remain < 0:
+                return
+            
+            for i in range(start, len(candidates)):
+                path.append(candidates[i])
+                dfs(i, remain - candidates[i])
+                path.pop()
+        
+        dfs(0, target)
+        return results
