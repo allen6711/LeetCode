e6b0650e55c367ddae27f461448e8a4261d84c60
allen6711 <allen2990167@gmail.com>
2026-06-20 23:38:57 -0700

    Time: 1 ms (45.29%), Space: 19.3 MB (96.81%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        path = []
+        visited = [False] * len(nums)
+        
+        def dfs():
+            if len(path) == len(nums):
+                results.append(path[:])
+                return
+            
+            for i in range(len(nums)):
+                if visited[i]:
+                    continue
+                
+                path.append(nums[i])
+                visited[i] = True
+                dfs()
+                path.pop()
+                visited[i] = False
+        
+        dfs()
+        return results
+
+                
