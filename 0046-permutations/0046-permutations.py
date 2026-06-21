ce3ba1a5cd2ce0a679228641fe37b40282289057
allen6711 <allen2990167@gmail.com>
2026-06-21 00:06:52 -0700

    Time: 1 ms (45.29%), Space: 19.7 MB (10.14%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -3,6 +3,40 @@ class Solution:
         results = []
         path = []
         visited = [False] * len(nums)
+
+        def dfs():
+            if len(path) == len(nums):
+                results.append(path[:])
+                return
+            
+            for i in range(len(nums)):
+                if visited[i]:
+                    continue
+                path.append(nums[i])
+                visited[i] = True
+                dfs()
+                path.pop()
+                visited[i] = False
+        
+        dfs()
+        return results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        results = []
+        path = []
+        visited = [False] * len(nums)
         
         def dfs():
             if len(path) == len(nums):
@@ -21,5 +55,3 @@ class Solution:
         
         dfs()
         return results
-
-                
