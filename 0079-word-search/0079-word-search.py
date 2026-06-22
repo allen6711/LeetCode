d7bef551b10870b13f283f7942cb4385b9456c21
allen6711 <allen2990167@gmail.com>
2026-06-22 01:33:34 -0700

    Time: 3979 ms (47.81%), Space: 19.3 MB (82.6%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,14 +1,17 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        # L: length of word
+        # O(m * n * 3^L)
+        # O(L)
         rows, cols = len(board), len(board[0])
 
-        def dfs(r: int, c: int, index: int) -> None:
+        def dfs(r: int, c: int, index: int) -> bool:
             if index == len(word):
                 return True
             
             if r < 0 or r >= rows or c < 0 or c >= cols:
                 return False
-                
+
             if board[r][c] != word[index]:
                 return False
             
