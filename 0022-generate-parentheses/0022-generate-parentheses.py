bdab82bab595100759f480444dbf754e4bb954a3
allen6711 <allen2990167@gmail.com>
2026-06-22 00:21:00 -0700

    Time: 3 ms (30.9%), Space: 19.2 MB (99.14%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -20,3 +20,38 @@ class Solution:
         
         dfs(0, 0)
         return results
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # O(n * C_n)
+        # O(n * C_n)
+        results = []
+        path = []
+
+        def dfs(open_count: int, close_count: int) -> None:
+            if len(path) == 2 * n:
+                results.append("".join(path))
+                return
+            
+            if open_count < n:
+                path.append("(")
+                dfs(open_count + 1, close_count)
+                path.pop()
+            
+            if close_count < open_count:
+                path.append(")")
+                dfs(open_count, close_count + 1)
+                path.pop()
+        
+        dfs(0, 0)
+        return results
