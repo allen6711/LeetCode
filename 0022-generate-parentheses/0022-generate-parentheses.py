18c735192de3ea716f20d3b010d3e5d32864ddfa
allen6711 <allen2990167@gmail.com>
2026-06-22 00:21:12 -0700

    Time: 0 ms (100%), Space: 19.3 MB (73.04%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,38 +1,5 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        results = []
-        path = []
-
-        def dfs(open_count: int, close_count: int) -> None:
-            if len(path) == 2 * n:
-                results.append("".join(path))
-                return
-            
-            if open_count < n:
-                path.append("(")
-                dfs(open_count + 1, close_count)
-                path.pop()
-            
-            if close_count < open_count:
-                path.append(")")
-                dfs(open_count, close_count + 1)
-                path.pop()
-        
-        dfs(0, 0)
-        return results
-
-
-
-
-
-
-
-
-
-
-
-
-
         # O(n * C_n)
         # O(n * C_n)
         results = []
