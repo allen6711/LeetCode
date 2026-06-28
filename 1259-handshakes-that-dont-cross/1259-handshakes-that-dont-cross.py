d16ccd91d860f8bb3db2920114f7546fab69cb41
allen6711 <allen2990167@gmail.com>
2026-06-27 23:39:41 -0700

    Time: 292 ms (42.68%), Space: 19.2 MB (94.89%) - LeetHub

---
+++ 1259-handshakes-that-dont-cross/1259-handshakes-that-dont-cross.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfWays(self, numPeople: int) -> int:
+        MOD = 10 ** 9 + 7
+        pairs = numPeople // 2
+        dp = [0] * (pairs + 1)
+
+        dp[0] = 1
+
+        for i in range(1, pairs + 1):
+            for j in range(i):
+                dp[i] += dp[j] * dp[i - 1 - j]
+                dp[i] %= MOD
+        
+        return dp[pairs]
