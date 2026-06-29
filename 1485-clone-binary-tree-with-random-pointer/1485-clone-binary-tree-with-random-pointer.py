411cf9179057ef21503d15b6a21f9e894bd7be9c
allen6711 <allen2990167@gmail.com>
2026-06-29 06:46:30 -0700

    Time: 140 ms (44.37%), Space: 24.8 MB (68.71%) - LeetHub

--- 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.py
+++ 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.py
@@ -8,6 +8,32 @@
 
 class Solution:
     def copyRandomBinaryTree(self, root: 'Optional[Node]') -> 'Optional[NodeCopy]':
+        old_to_new = {}
+        
+        def dfs(node: 'Optional[Node]') -> 'Optional[Node]':
+            if not node:
+                return None
+            if node in old_to_new:
+                return old_to_new[node]
+            
+            copy = NodeCopy(node.val)
+            old_to_new[node] = copy
+
+            copy.left = dfs(node.left)
+            copy.right = dfs(node.right)
+            copy.random = dfs(node.random)
+
+            return copy
+        
+        return dfs(root)
+
+
+
+
+
+
+
+        
         # O(n)
         # O(n)
         old_to_new = {}
