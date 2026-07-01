ce0fa617bf27d9ef0d1c8941b42976f97a42ea4a
allen6711 <allen2990167@gmail.com>
2026-07-02 00:19:21 +0800

    Time: 97 ms (65.6%), Space: 19.4 MB (84.97%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -5,11 +5,10 @@ class Solution:
             "b": 0,
             "c": 0
         }
-
         answer = 0
         left = 0
         n = len(s)
-        for right in range(len(s)):
+        for right in range(n):
             count[s[right]] += 1
             while count["a"] > 0 and count["b"] > 0 and count["c"] > 0:
                 answer += n - right
