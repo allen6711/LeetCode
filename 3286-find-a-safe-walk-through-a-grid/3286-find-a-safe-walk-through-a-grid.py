efe740206a5fcd18fcbbf6ddebbb5fdc4b538f87
allen6711 <allen2990167@gmail.com>
2026-07-02 23:32:16 +0800

    Time: 59 ms (87.74%), Space: 19.6 MB (61.79%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.py
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.py
@@ -3,6 +3,39 @@ class Solution:
         m, n = len(grid), len(grid[0])
         dist = [[float('inf')] * n for _ in range(m)]
         dist[0][0] = grid[0][0]
+        queue = deque([(0, 0)])
+
+        while queue:
+            r, c = queue.popleft()
+            for dr, dc in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n:
+                    new_cost = dist[r][c] + grid[nr][nc]
+
+                    if new_cost < dist[nr][nc]:
+                        dist[nr][nc] = new_cost
+                        if grid[nr][nc] == 0:
+                            queue.appendleft((nr, nc))
+                        else:
+                            queue.append((nr, nc))
+        
+        return dist[m - 1][n - 1] < health
+
+
+
+
+
+
+
+
+
+
+
+        # O(m*n)
+        # O(m*n)
+        m, n = len(grid), len(grid[0])
+        dist = [[float('inf')] * n for _ in range(m)]
+        dist[0][0] = grid[0][0]
 
         queue = deque([(0, 0)])
         
@@ -20,5 +53,5 @@ class Solution:
                             queue.appendleft((nr, nc))
                         else:
                             queue.append((nr, nc))
-                            
+
         return dist[m - 1][n - 1] < health
