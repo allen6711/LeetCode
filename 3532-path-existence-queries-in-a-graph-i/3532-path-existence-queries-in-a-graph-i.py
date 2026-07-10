254024f79607a3de19462503d87c47a9003d8b03
allen6711 <allen2990167@gmail.com>
2026-07-10 23:39:32 +0800

    Time: 71 ms (78.23%), Space: 50.3 MB (45.16%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
@@ -10,6 +10,36 @@ class Solution:
             component[i] = component_id
         
         answer = []
+        for u, v in queries:
+            answer.append(component[u] == component[v])
+        
+        return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        component = [0] * n
+        component_id = 0
+
+        for i in range(1, n):
+            if nums[i] - nums[i - 1] > maxDiff:
+                component_id += 1
+            
+            component[i] = component_id
+        
+        answer = []
 
         for u, v in queries:
             answer.append(component[u] == component[v])
