7c768d821d5b969bf6aec613b2a1c2749dbb3966
allen6711 <allen2990167@gmail.com>
2026-07-12 20:38:55 +0800

    Time: 36 ms (66.7%), Space: 37.6 MB (60.61%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,15 +1,14 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        # O(nlogn)
+        # O(n)
         sorted_value = sorted(set(arr))
         rank = {}
         answer = []
         for index, value in enumerate(sorted_value):
-            if value not in rank:
-                rank[value] = index + 1
+            rank[value] = index + 1
         
         for value in arr:
             answer.append(rank[value])
         
         return answer
-
-
