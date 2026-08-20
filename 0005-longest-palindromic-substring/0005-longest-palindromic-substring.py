6ab424e147d20a15bd3e171464d7281d2931813b
allen6711 <allen2990167@gmail.com>
2026-08-20 19:44:22 +0800

    Time: 295 ms (45.33%), Space: 19.1 MB (91.73%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -2,10 +2,26 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         # O(n)
         # O(1)
-        # max_len = 0
-        # n = len(s)
-        # start = 0
-        # def expand(left: int, right: int)
+        max_length = 0
+        n = len(s)
+        start = 0
+        def expand(left: int, right: int):
+            nonlocal max_length, start
+            while left >= 0 and right < n and s[left] == s[right]:
+                cur_length = right - left + 1
+                if cur_length > max_length:
+                    start = left
+                    max_length = cur_length
+                
+                left -= 1
+                right += 1
+
+        for i in range(n):
+            expand(i, i)
+            expand(i, i + 1)
+        
+        return s[start:start + max_length]
+
         # O(n)
         # O(n)
         max_len = 0
@@ -26,6 +42,6 @@ class Solution:
             # Odd
             expand(i, i)
             # Even
-            extand(i, i + 1)
+            expand(i, i + 1)
         
         return ans
