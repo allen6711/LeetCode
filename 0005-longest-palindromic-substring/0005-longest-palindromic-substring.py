1767dd4f235be8b407f59381fd0dbeef25bce56e
allen6711 <allen2990167@gmail.com>
2026-08-20 19:40:53 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,9 +1,17 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        # O(n)
+        # O(1)
+        # max_len = 0
+        # n = len(s)
+        # start = 0
+        # def expand(left: int, right: int)
+        # O(n)
+        # O(n)
         max_len = 0
         n = len(s)
         ans = ""
-        def extand(left: int, right: int) -> str:
+        def expand(left: int, right: int):
             nonlocal max_len, ans
             while left >= 0 and right < n and s[left] == s[right]:
                 cur_length = right - left + 1
@@ -16,74 +24,8 @@ class Solution:
         
         for i in range(n):
             # Odd
-            extand(i, i)
+            expand(i, i)
             # Even
             extand(i, i + 1)
         
         return ans
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-        res = ""
-        n = len(s)
-
-        def expand(left, right):
-            nonlocal res
-
-            while left >= 0 and right < n and s[left] == s[right]:
-                if right - left + 1 > len(res):
-                    res = s[left:right + 1]
-                
-                left -= 1
-                right += 1
-        
-        for i in range(n):
-            # odd-length palindrome
-            expand()
-
-        return res
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        n = len(s)
-        dp = [[True] * n for _ in range(n)]
-        max_length = 1
-        start_index = 0
-
-        for i in range(n - 2, -1, -1):
-            for j in range(i + 1, n):
-                dp[i][j] = False
-
-                if s[i] == s[j] and dp[i + 1][j - 1]:
-                    dp[i][j] = True
-                    if max_length < j - i + 1:
-                        start_index = i
-                        max_length = j - i + 1
-        
-        return s[start_index: start_index + max_length]
