94c8da0ed52aea959b85374ef1ab67690d0818ec
allen6711 <allen2990167@gmail.com>
2026-09-09 08:46:06 +0800

    Time: 0 ms (100%), Space: 19.5 MB (10.7%) - LeetHub

---
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minOperations(self, n: int) -> int:
+        # n += 2^i
+        # n -= 2^i
+        # In binary situation
+        # Single 1, Consecutive 1
+        operations = 0
+
+        while n > 0:
+            if n % 2 == 0:
+                n >>= 1
+            
+            # .....11
+            elif n & 3 == 3:
+                n += 1
+                operations += 1
+            
+            else:
+                n -= 1
+                operations += 1
+        
+        return operations
