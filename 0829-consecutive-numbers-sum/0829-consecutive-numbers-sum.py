07759ce27f1561db52da38f3fce8e03067d7df0b
allen6711 <allen2990167@gmail.com>
2026-09-10 22:01:31 -0700

    Time: 111 ms (25%), Space: 19.3 MB (69.35%) - LeetHub

---
+++ 0829-consecutive-numbers-sum/0829-consecutive-numbers-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def consecutiveNumbersSum(self, n: int) -> int:
+        # k: consecutive numbers
+        # x + (x + 1) + (x + 2) + ... + (x + k - 1)
+        # sum = n = [(x + x + k - 1) * k] / 2
+        # n = [(2x + k - 1) * k] / 2
+        # [(2n / k) + 1 - k] = x
+        # x = [(2n + k - k^2)] / k
+        # x = [2n - k(k - 1)] / k
+        ans = 0 # x
+        k = 1
+
+        while k * (k - 1) // 2 < n:
+            remain = n - k * (k - 1) // 2
+            if remain % k == 0:
+                ans += 1
+            
+            k += 1
+        
+        return ans
