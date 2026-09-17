7df759d1e073bc5653c8a65f5abfae77cc0df953
allen6711 <allen2990167@gmail.com>
2026-09-17 14:38:28 -0700

    Time: 75 ms (88.01%), Space: 19.7 MB (52.8%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -16,22 +16,16 @@ class Solution:
         # O(n)
         # O(1)
         n = len(s)
-        count = {}
-        max_freq = 0
-        best = 0
+        counts = defaultdict(int)
+        max_count = 0
         left = 0
+        ans = 0
         for right in range(n):
-            char = s[right]
-            # Expanding counting logic
-            if char in count:
-                count[char] += 1
-            else:
-                count[char] = 1
-            if count[char] > max_freq:
-                max_freq = count[char]
-            while (right - left + 1 - max_freq) > k:
-                left_char = s[left]
-                count[left_char] -= 1
+            counts[s[right]] += 1
+            max_count = max(max_count, counts[s[right]])
+            while (right - left + 1) - max_count > k:
+                counts[s[left]] -= 1
                 left += 1
-            best = max(best, right - left + 1)
-        return best  
+            ans = max(ans, right - left + 1)
+        
+        return ans
