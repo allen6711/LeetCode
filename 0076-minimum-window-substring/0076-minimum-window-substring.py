5b691046f3d8952a9733e53dab25572666617a90
allen6711 <allen2990167@gmail.com>
2026-09-17 23:07:13 -0700

    Time: 63 ms (73.21%), Space: 19.7 MB (38.02%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,31 +1,5 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        n = len(s)
-        left = 0
-        best_len = float('inf')
-        best_left = 0
-        need = Counter(t)
-        window = defaultdict(int)
-        have = 0
-        for right, char in enumerate(s):
-            window[char] += 1
-            if char in need and window[char] == need[char]:
-                have += 1
-            while have == len(need):
-                if right - left + 1 < best_len:
-                    best_len = right - left + 1
-                    best_left = left
-                
-                # Explore next
-                left_char = s[left]
-                window[left_char] -= 1
-                if left_char in need and window[left_char] < need[left_char]:
-                    have -= 1
-                left += 1
-        
-        return "" if best_len == float('inf') else s[best_left:best_left + best_len]
-
-
         # O(n^3)
         # O(k)
         # if not s or not t:
@@ -54,36 +28,35 @@ class Solution:
         # return best_sub
         # O(n) (O(|s)+|t|)
         # O(k)
-        if not s or not t:
-            return ""
         need = Counter(t)
         window = defaultdict(int)
-        have = 0
-        need_char_num = len(need)
-        best_len = float('inf')
-        best_left = 0
+
+        required = len(need)
+        formed = 0
+
         left = 0
+        min_len = float('inf')
+        min_left = 0
 
         for right, char in enumerate(s):
             window[char] += 1
 
             # The character reached the required count
             if char in need and window[char] == need[char]:
-                have += 1
+                formed += 1
             
             # Try to shrink while the window is valid
-            while have == need_char_num:
-                window_len = right - left + 1
-                if window_len < best_len:
-                    best_len = window_len
-                    best_left = left
+            while formed == required:
+                if right - left + 1 < min_len:
+                    min_len  = right - left + 1
+                    min_left = left
                 
                 # Pop from the left
                 left_char = s[left]
                 window[left_char] -= 1
                 # Window becomes invalid after removing left_char
                 if left_char in need and window[left_char] < need[left_char]:
-                    have -= 1
+                    formed -= 1
                 left += 1
         
-        return "" if best_len == float('inf') else s[best_left:best_left + best_len]
+        return "" if min_len == float('inf') else s[min_left:min_left + min_len]
