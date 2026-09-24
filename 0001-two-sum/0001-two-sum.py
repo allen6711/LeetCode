ee12f644d69b8a300922a9d05ef5e67eb58809ff
allen6711 <allen2990167@gmail.com>
2026-09-24 10:51:39 -0700

    Time: 4 ms (40.99%), Space: 20.4 MB (59.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,26 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         visited = defaultdict(int)
-        for i, num in enumerate(nums):
-            if target - num in visited:
-                return [visited[target - num], i]
-            visited[num] = i
-        
-        return []
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         # O(n^2)
         # O(1)
         # n = len(nums)
