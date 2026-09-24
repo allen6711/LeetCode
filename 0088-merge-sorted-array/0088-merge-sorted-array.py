c6468d74627c87af45fc0e48a7e4e568e1156621
allen6711 <allen2990167@gmail.com>
2026-09-24 12:28:06 -0700

    Time: 2 ms (7.53%), Space: 19.1 MB (95.89%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,10 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # O((m+n)log(m+n))
+        # O(1)
+        # for i in range(n):
+        #     nums1[m + i] = nums2[i]
+        
+        # nums1.sort()
+        # O(m+n)
+        # O(1)
         left = m - 1
         right = n - 1
         middle = m + n - 1
-
         while left >= 0 and right >= 0:
             if nums1[left] > nums2[right]:
                 nums1[middle] = nums1[left]
@@ -16,46 +23,9 @@ class Solution:
                 right -= 1
             middle -= 1
         
+        # Only copy the remaining elements from nums2.
+        # Any remaining elements in nums1 are already in the correct position
         while right >= 0:
             nums1[middle] = nums2[right]
             right -= 1
             middle -= 1
-        
-        return nums1
-            
-
-
-
-
-
-
-
-
-
-
-        # O((m+n)log(m+n))
-        # O(1)
-        # for i in range(n):
-        #     nums1[m + i] = nums2[i]
-        
-        # nums1.sort()
-        # O(m+n)
-        # O(1)
-        i = m - 1
-        j = n - 1
-        k = m + n - 1
-        while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
-            else:
-                nums1[k] = nums2[j]
-                j -= 1
-            k -= 1
-        
-        # Only copy the remaining elements from nums2.
-        # Any remaining elements in nums1 are already in the correct position
-        while j >= 0:
-            nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
