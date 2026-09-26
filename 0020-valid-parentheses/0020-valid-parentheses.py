9b33464f983880bec44a488fd86defef0e9f3d8d
allen6711 <allen2990167@gmail.com>
2026-09-26 09:04:28 -0700

    Time: 0 ms (100%), Space: 19.3 MB (64.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,5 +1,39 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        # O(n)
+        # O()
+        # 因為括號要遵守：最後打開的括號，要最先被關閉。
+        # 也就是：Last In, First Out (LIFO)
+        pair = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for char in s:
+            if char in "([{":
+                stack.append(char)
+            
+            else:
+                if not stack or stack[-1] != pair[char]:
+                    return False
+                
+                stack.pop()
+        
+        return len(stack) == 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
         expected_dict = {")": "(", "]": "[", "}": "{"}
         stack = []
 
